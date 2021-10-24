b39315a00ef69f5d7189bc1828cbb1100f61f543
Maikel Bos <maikel.bos0@gmail.com>
2021-10-24 10:49:11 +0200

    First full proof of concept of passing event

---
+++ BlazorPlayground/wwwroot/js/globaleventmanager.js
@@ -0,0 +1,16 @@
+﻿var GlobalEventManager = {};
+
+GlobalEventManager.register = function (dotNetObjectReference) {
+    function keyDownHandler(e) {
+        // TODO expand with full keydown event args
+        dotNetObjectReference.invokeMethodAsync("OnKeyDown", {
+            key: e.key
+        })
+            .catch(function (e) {
+                console.log(e);
+                window.removeEventListener("keydown", keyDownHandler);
+            });
+    }
+
+    window.addEventListener("keydown", keyDownHandler);
+}
