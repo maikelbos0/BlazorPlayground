92dda1eb7339a6c1fb70bfa07c2ceeea41d4c2c5
Maikel Bos <maikel.bos0@gmail.com>
2021-11-15 10:18:09 +0100

    Add test form with validation

---
+++ BlazorPlayground/Components/Model.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorPlayground.Components {
+    public class Model {
+        [Required(ErrorMessage = "Value is required")]
+        [MinLength(1, ErrorMessage = "Value is required")]
+        public string Value { get; set; }
+    }
+}
