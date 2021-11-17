584a24e9fe9407999f944856329f109ee30f1c03
Maikel Bos <maikel.bos0@gmail.com>
2021-11-17 21:00:31 +0100

    Inherit from wizard step instead of inheritance

---
+++ BlazorPlayground/Components/WizardStep.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace BlazorPlayground.Components {
+    public partial class WizardStep : ComponentBase {
+        [CascadingParameter]
+        private Wizard Parent { get; set; }
+
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+        public EventCallback<InitializeStepEventArgs> OnInitializeStep { get; set; }
+
+        [Parameter]
+        public EventCallback<TryCompleteStepEventArgs> OnTryCompleteStep { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        public bool IsActive => Parent.ActiveStep == this;
+
+        protected override async Task OnInitializedAsync() {
+            await Parent.AddStep(this);
+        }
+
+        internal virtual async Task<InitializeStepEventArgs> InitializeStep(InitializeStepEventArgs args) {
+            await OnInitializeStep.InvokeAsync(args);
+
+            return args;
+        }
+
+        internal virtual async Task<TryCompleteStepEventArgs> TryCompleteStep(TryCompleteStepEventArgs args) {
+            await OnTryCompleteStep.InvokeAsync(args);
+
+            return args;
+        }
+    }
+}
