0d290ff73dd46b07cfc130a4343544c9e53e7f6e
Maikel Bos <maikel.bos0@gmail.com>
2022-01-11 17:03:20 +0100

    Set up basic calculation expression

---
+++ BlazorPlayground.Calculator/CalculationExpression.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorPlayground.Calculator {
+    public class CalculationExpression {
+        private readonly List<char> characters = new List<char>();
+
+        public bool AcceptDigit => false;
+
+        public bool AcceptDecimalSeparator => false;
+
+        public bool AcceptBinaryOperator => false;
+
+        public bool AcceptUnaryOperator => false;
+        
+        public bool AcceptOpeningParenthesis => false;
+        
+        public bool AcceptClosingParenthesis => false;
+
+        public string Display => new string(characters.ToArray());
+
+        public bool TryAppend(char c) {
+            characters.Add(c);
+
+            return true;
+        }
+
+        public void Clear() {
+            characters.Clear();
+        }
+
+        override public string ToString() => Display;
+    }
+}
