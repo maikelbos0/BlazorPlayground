dca5b83e394bd737e667d88dbec877f683add2d6
Maikel Bos <maikel.bos0@gmail.com>
2022-01-16 10:50:23 +0100

    Calculation expression decimal char tests

--- BlazorPlayground.Calculator.Tests/CalculationExpressionTests.cs
+++ BlazorPlayground.Calculator.Tests/CalculationExpressionTests.cs
@@ -18,5 +18,21 @@ namespace BlazorPlayground.Calculator.Tests {
 
             Assert.Equal(expected, expression.AcceptDigit);
         }
+
+        [Theory]
+        [InlineData("", true)]
+        [InlineData("1", true)]
+        [InlineData("1.", false)]
+        [InlineData("1.1", false)]
+        [InlineData("1.1+", true)]
+        [InlineData("(", true)]
+        [InlineData("(1", true)]
+        [InlineData("(1)", false)]
+        [InlineData("-", true)]
+        public void CalculationExpression_AcceptDecimalSeparator(string value, bool expected) {
+            var expression = new CalculationExpression(value);
+
+            Assert.Equal(expected, expression.AcceptDecimalSeparator);
+        }
     }
 }
