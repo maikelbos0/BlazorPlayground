b711d73fe744738fa429cca8adbcd0ea3a4d9cca
Maikel Bos <maikel.bos0@gmail.com>
2022-01-17 11:48:54 +0100

    Add quick number evaluate test for completeness

---
+++ BlazorPlayground.Calculator.Tests/NumberTests.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace BlazorPlayground.Calculator.Tests {
+    public class NumberTests {
+        [Fact]
+        public void Number_Evaluate_Returns_Value() {
+            var number = new Number(12.3M);
+
+            Assert.Equal(12.3M, number.Evaluate());
+        }
+    }
+}
