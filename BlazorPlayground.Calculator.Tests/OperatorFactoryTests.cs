37a50b531d8ea1ee933eb782da48031f4b05775b
Maikel Bos <maikel.bos0@gmail.com>
2022-01-18 11:56:19 +0100

    Add operator factory

---
+++ BlazorPlayground.Calculator.Tests/OperatorFactoryTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+
+namespace BlazorPlayground.Calculator.Tests {
+    public class OperatorFactoryTests {
+        [Theory]
+        [InlineData('/', typeof(DivisionOperator))]
+        [InlineData('÷', typeof(DivisionOperator))]
+        [InlineData('*', typeof(MultiplicationOperator))]
+        [InlineData('×', typeof(MultiplicationOperator))]
+        [InlineData('-', typeof(SubtractionOperator))]
+        [InlineData('−', typeof(SubtractionOperator))]
+        [InlineData('+', typeof(AdditionOperator))]
+        public void OperatorFactory_Creates_Correct_Operator(char character, Type expectedType) {
+            var op = OperatorFactory.GetOperator(character);
+
+            Assert.IsType(expectedType, op);
+        }
+
+        [Theory]
+        [InlineData('0')]
+        [InlineData('9')]
+        [InlineData('.')]
+        [InlineData(',')]
+        [InlineData('±')]
+        public void OperatorFactory_Does_Not_Create_Operator_For_Numeric_Value(char character) {
+            var op = OperatorFactory.GetOperator(character);
+
+            Assert.Null(op);
+        }
+    }
+}
---
+++ BlazorPlayground.Calculator/OperatorFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorPlayground.Calculator {
+    internal static class OperatorFactory {
+        public static Operator? GetOperator(char character) => character switch {
+            '/' => new DivisionOperator(),
+            '÷' => new DivisionOperator(),
+            '*' => new MultiplicationOperator(),
+            '×' => new MultiplicationOperator(),
+            '-' => new SubtractionOperator(),
+            '−' => new SubtractionOperator(),
+            '+' => new AdditionOperator(),
+            _ => null
+        };
+    }
+}
