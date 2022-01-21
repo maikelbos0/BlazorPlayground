0502c7d4c67c5568aac1fe4110714555f6bd54be
Maikel Bos <maikel.bos0@gmail.com>
2022-01-21 11:37:50 +0100

    Add first symbol TryAppendTo tests

--- BlazorPlayground.Calculator.Tests/NegationOperatorTests.cs
+++ BlazorPlayground.Calculator.Tests/NegationOperatorTests.cs
@@ -1,4 +1,6 @@
-﻿using Xunit;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
 
 namespace BlazorPlayground.Calculator.Tests {
     public class NegationOperatorTests {
@@ -21,6 +23,44 @@ namespace BlazorPlayground.Calculator.Tests {
             };
 
             Assert.Equal("-(5.5)", op.ToString());
+        }
+
+        [Fact]
+        public void NegationOperator_TryAppendTo_Succeeds_When_Previous_Symbol_Is_Evaluatable() {
+            var op = new NegationOperator();
+            var number = new LiteralNumber(5.5M);
+            var symbols = new List<ISymbol>() {
+                number
+            };
+
+            Assert.True(op.TryAppendTo(symbols));
+            Assert.Equal(op, Assert.Single(symbols));
+            Assert.Equal(number, op.Symbol);
+        }
+
+        [Fact]
+        public void NegationOperator_TryAppendTo_Fails_When_Symbols_Is_Empty() {
+            var op = new NegationOperator();
+            var symbols = new List<ISymbol>();
+
+            Assert.False(op.TryAppendTo(symbols));
+            Assert.Empty(symbols);
+            Assert.Null(op.Symbol);
+        }
+
+        [Fact]
+        public void NegationOperator_TryAppendTo_Fails_When_Previous_Symbol_Is_Not_Evaluatable() {
+            var op = new NegationOperator();
+            var binaryOp = new AdditionOperator('+');
+            var symbols = new List<ISymbol>() {
+                new LiteralNumber(5.5M),
+                binaryOp
+            };
+
+            Assert.False(op.TryAppendTo(symbols));
+            Assert.Equal(2, symbols.Count);
+            Assert.Equal(binaryOp, symbols.Last());
+            Assert.Null(op.Symbol);
         }
     }
 }
