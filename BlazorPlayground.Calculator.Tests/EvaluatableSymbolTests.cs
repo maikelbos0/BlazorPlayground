76bc1f2c791e8195cc17bfb8c6a65f24165e6df2
Maikel Bos <maikel.bos0@gmail.com>
2022-01-21 12:26:50 +0100

    Add TryAppendTo implementation to EvaluatableSymbol

---
+++ BlazorPlayground.Calculator.Tests/EvaluatableSymbolTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace BlazorPlayground.Calculator.Tests {
+    public class EvaluatableSymbolTests {
+        [Fact]
+        public void SymbolGroup_Can_Append_EvaluatableSymbol_When_Empty() {
+            var number = new LiteralNumber(0);
+            var symbols = new List<ISymbol>();
+
+            Assert.True(number.TryAppendTo(symbols));
+            Assert.Equal(number, Assert.Single(symbols));
+        }
+
+        [Fact]
+        public void SymbolGroup_Can_Append_EvaluatableSymbol_When_Last_Symbol_Is_BinaryOperator() {
+            var number = new LiteralNumber(0);
+            var symbols = new List<ISymbol>() {
+                new LiteralNumber(1),
+                new AdditionOperator('+')
+            };
+
+            Assert.True(number.TryAppendTo(symbols));
+            Assert.Equal(3, symbols.Count);
+            Assert.Equal(number, symbols[2]);
+        }
+
+        [Fact]
+        public void SymbolGroup_Can_Not_Append_EvaluatableSymbol_When_Last_Symbol_Is_EvaluatableSymbol() {
+            var group = new SymbolGroup();
+            var literalNumber = new LiteralNumber(1);
+
+            group.Symbols.Add(new LiteralNumber(1));
+
+            Assert.False(group.Append(literalNumber));
+            Assert.Single(group.Symbols);
+        }
+    }
+}
--- BlazorPlayground.Calculator/EvaluatableSymbol.cs
+++ BlazorPlayground.Calculator/EvaluatableSymbol.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace BlazorPlayground.Calculator {
     internal abstract class EvaluatableSymbol : ISymbol {
         public virtual bool TryAppendTo(IList<ISymbol> symbols) {
-            throw new System.NotImplementedException();
+            if (symbols.Count == 0 || symbols.Last() is BinaryOperator) {
+                symbols.Add(this);
+                return true;
+            }
+
+            return false;
         }
 
         internal abstract decimal Evaluate();
