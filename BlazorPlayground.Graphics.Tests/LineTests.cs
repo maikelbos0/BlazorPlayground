3157d26da9578a93d87a757a312ecf19ecbb09f6
Maikel Bos <maikel.bos0@gmail.com>
2022-07-11 10:15:47 +0200

    Add rectangle

--- BlazorPlayground.Graphics.Tests/LineTests.cs
+++ BlazorPlayground.Graphics.Tests/LineTests.cs
@@ -14,4 +14,18 @@ namespace BlazorPlayground.Graphics.Tests {
             PointAssert.Equal(new Point(200, 250), result[1]);
         }
     }
+    public class RectangleTests {
+        [Fact]
+        public void GetPoints() {
+            var rectangle = new Rectangle(new Point(100, 150), new Point(200, 250));
+
+            var result = rectangle.GetPoints().ToList();
+
+            Assert.Equal(4, result.Count);
+            PointAssert.Equal(new Point(100, 150), result[0]);
+            PointAssert.Equal(new Point(100, 250), result[1]);
+            PointAssert.Equal(new Point(200, 250), result[2]);
+            PointAssert.Equal(new Point(200, 150), result[3]);
+        }
+    }
 }
---
+++ BlazorPlayground.Graphics/Rectangle.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorPlayground.Graphics {
+    public class Rectangle : Shape {
+        public override ShapeRenderType RenderType => ShapeRenderType.Polygon;
+        public Point StartPoint { get; set; }
+        public Point EndPoint { get; set; }
+
+        public Rectangle(Point startPoint, Point endPoint) {
+            StartPoint = startPoint;
+            EndPoint = endPoint;
+        }
+
+        public override IEnumerable<Point> GetPoints() {
+            yield return StartPoint;
+            yield return new Point(StartPoint.X, EndPoint.Y);
+            yield return EndPoint;
+            yield return new Point(EndPoint.X, StartPoint.Y);
+        }
+    }
+}
