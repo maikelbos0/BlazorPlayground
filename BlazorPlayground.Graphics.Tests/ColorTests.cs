dbd7437083fbd9f04d567efb96157756fae83db4
Maikel Bos <maikel.bos0@gmail.com>
2022-07-17 16:38:26 +0200

    Add first version of color class for parsing colors

---
+++ BlazorPlayground.Graphics.Tests/ColorTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace BlazorPlayground.Graphics.Tests {
+    public class ColorTests {
+        [Theory]
+        [InlineData("red", "red", 255, 0, 0, 1)]
+        public void CastFromString(string value, string expectedValue, byte expectedRed, byte expectedGreen, byte expectedBlue, double expectedAlpha) {
+            Color color = value;
+
+            Assert.Equal(expectedValue, color.ColorValue);
+            Assert.Equal(expectedRed, color.Red);
+            Assert.Equal(expectedGreen, color.Green);
+            Assert.Equal(expectedBlue, color.Blue);
+            Assert.Equal(expectedAlpha, color.Alpha);
+        }
+    }
+}
---
+++ BlazorPlayground.Graphics/Color.cs
@@ -0,0 +1,25 @@
+﻿namespace BlazorPlayground.Graphics {
+    public class Color {
+        public string ColorValue { get; }
+        public byte Red { get; }
+        public byte Green { get; }
+        public byte Blue { get; }
+        public double Alpha { get; }
+
+        public Color(string colorValue, byte red, byte green, byte blue, double alpha) {
+            ColorValue = colorValue;
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Alpha = alpha;
+        }
+
+        public override string ToString() => ColorValue;
+
+        public static implicit operator Color(string colorValue) {
+            var color = System.Drawing.Color.FromName(colorValue);
+
+            return new Color(colorValue, color.R, color.G, color.B, color.A / 255.0);
+        }
+    }
+}
