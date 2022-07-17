58cbb51cfcb7003bc1e7b3fa245a203d4de8dbc3
Maikel Bos <maikel.bos0@gmail.com>
2022-07-17 16:55:35 +0200

    Improve color

--- BlazorPlayground.Graphics.Tests/ColorTests.cs
+++ BlazorPlayground.Graphics.Tests/ColorTests.cs
@@ -4,9 +4,10 @@ namespace BlazorPlayground.Graphics.Tests {
     public class ColorTests {
         [Theory]
         [InlineData("red", "red", 255, 0, 0, 1)]
-        [InlineData("unknown", "unknown", 0, 0, 0, 1)]
         [InlineData("#ffccdd", "#ffccdd", 255, 204, 221, 1)]
         [InlineData("#fcd", "#fcd", 255, 204, 221, 1)]
+        [InlineData("unknown", "unknown", 0, 0, 0, 1)]
+        [InlineData("#fffff", "#fffff", 0, 0, 0, 1)]
         public void CastFromString(string value, string expectedValue, byte expectedRed, byte expectedGreen, byte expectedBlue, double expectedAlpha) {
             Color color = value;
 
--- BlazorPlayground.Graphics/Color.cs
+++ BlazorPlayground.Graphics/Color.cs
@@ -15,11 +15,11 @@ namespace BlazorPlayground.Graphics {
 
         private readonly static Parser[] parsers = new[] {
             new Parser(
-                new Regex("^#(?<Red>[0-9a-fA-Z]{2})(?<Green>[0-9a-fA-Z]{2})(?<Blue>[0-9a-fA-Z]{2})$"),
+                new Regex("^#(?<Red>[0-9a-f]{2})(?<Green>[0-9a-f]{2})(?<Blue>[0-9a-f]{2})$", RegexOptions.IgnoreCase | RegexOptions.Compiled),
                 (value, match) => new Color(value, byte.Parse(match.Groups["Red"].Value, NumberStyles.HexNumber), byte.Parse(match.Groups["Green"].Value, NumberStyles.HexNumber), byte.Parse(match.Groups["Blue"].Value, NumberStyles.HexNumber), 1)
             ),
             new Parser(
-                new Regex("^#(?<Red>[0-9a-fA-Z])(?<Green>[0-9a-fA-Z])(?<Blue>[0-9a-fA-Z])$"),
+                new Regex("^#(?<Red>[0-9a-f])(?<Green>[0-9a-f])(?<Blue>[0-9a-f])$", RegexOptions.IgnoreCase | RegexOptions.Compiled),
                 (value, match) => new Color(value, (byte)(byte.Parse(match.Groups["Red"].Value, NumberStyles.HexNumber) * 17), (byte)(byte.Parse(match.Groups["Green"].Value, NumberStyles.HexNumber) * 17), (byte)(byte.Parse(match.Groups["Blue"].Value, NumberStyles.HexNumber) * 17), 1)
             )
         };
@@ -42,14 +42,14 @@ namespace BlazorPlayground.Graphics {
 
         public static implicit operator Color(string colorValue) {
             foreach (var parser in parsers) {
-                var match = parser.Matcher.Match(colorValue);
+                var match = parser.Matcher.Match(colorValue.Trim());
 
                 if (match.Success) {
                     return parser.Constructor(colorValue, match);
                 }
             }
 
-            var color = System.Drawing.Color.FromName(colorValue);
+            var color = System.Drawing.Color.FromName(colorValue.Trim());
 
             return new Color(colorValue, color.R, color.G, color.B, 1);
         }
