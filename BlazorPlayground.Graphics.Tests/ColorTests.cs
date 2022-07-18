743247b78291a00d854db204a44432fff862be93
Maikel Bos <maikel.bos0@gmail.com>
2022-07-18 12:14:47 +0200

    Add contrasting color to color for display in forms

--- BlazorPlayground.Graphics.Tests/ColorTests.cs
+++ BlazorPlayground.Graphics.Tests/ColorTests.cs
@@ -3,6 +3,19 @@
 namespace BlazorPlayground.Graphics.Tests {
     public class ColorTests {
         [Theory]
+        [InlineData(255, 255, 255, 1, "#000000")]
+        [InlineData(0, 0, 0, 1, "#FFFFFF")]
+        [InlineData(128, 127, 127, 1, "#000000")]
+        [InlineData(127, 127, 127, 1, "#FFFFFF")]
+        [InlineData(0, 0, 0, 0.5, "#000000")]
+        [InlineData(0, 0, 0, 0.51, "#FFFFFF")]
+        public void ContrastingColor(byte red, byte green, byte blue, double alpha, string expectedColor) {
+            var color = new Color(red, green, blue, alpha);
+
+            Assert.Equal(expectedColor, color.ContrastingColor.ToString());
+        }
+
+        [Theory]
         [InlineData(255, 255, 255, 1, "#FFFFFF")]
         [InlineData(0, 0, 0, 1, "#000000")]
         [InlineData(255, 255, 255, 0.5, "rgba(255, 255, 255, 0.5)")]
