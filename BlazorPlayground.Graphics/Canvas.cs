b58f0fcaa768a6b05a0b63b2f69e0afd25f0e27a
Maikel Bos <maikel.bos0@gmail.com>
2022-07-27 19:50:45 +0200

    Start moving logic to canvas class

---
+++ BlazorPlayground.Graphics/Canvas.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorPlayground.Graphics {
+    public class Canvas {
+        private int width = 800;
+        private int height = 800;
+        private int gridSize = 50;
+
+        public int Width {
+            get => width;
+            set => width = Math.Max(value, 1);
+        }
+        public int Height {
+            get => height;
+            set => height = Math.Max(value, 1);
+        }
+        public int GridSize {
+            get => gridSize;
+            set => gridSize = Math.Max(value, 1);
+        }
+        public bool ShowGrid { get; set; } = false;
+        public bool SnapToGrid { get; set; } = false;
+    }
+}
