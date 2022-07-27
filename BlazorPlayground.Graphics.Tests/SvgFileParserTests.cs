f94bc72600b3beceea67e284fda697143e671f8d
Maikel Bos <maikel.bos0@gmail.com>
2022-07-27 19:28:52 +0200

    Add parsing of arbitrary data as raw shapes

---
+++ BlazorPlayground.Graphics.Tests/SvgFileParserTests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+
+namespace BlazorPlayground.Graphics.Tests {
+    public class SvgFileParserTests {
+        [Fact]
+        public void Parse_Invalid_Xml() {
+            var result = SvgFileParser.Parse("<>");
+
+            Assert.False(result.IsSuccess);
+            Assert.Equal("The provided file is not a valid svg file.", result.ErrorMessage);
+        }
+
+        [Fact]
+        public void Parse_Invalid_Svg() {
+            var result = SvgFileParser.Parse("<test></test>");
+
+            Assert.False(result.IsSuccess);
+            Assert.Equal("The provided file is not a valid svg file.", result.ErrorMessage);
+        }
+
+        [Fact]
+        public void Parse_Raw_Shape() {
+            var result = SvgFileParser.Parse("<svg><line x1=\"100\" y1=\"150\" x2=\"200\" y2=\"250\" stroke=\"black\" /></svg>");
+
+            Assert.True(result.IsSuccess);
+            
+            var shape = Assert.IsType<RawShape>(Assert.Single(result.Shapes));
+            var element = shape.CreateElement();
+
+            Assert.Equal("line", element.Name);
+            Assert.Equal("100", element.Attribute("x1")?.Value);
+            Assert.Equal("150", element.Attribute("y1")?.Value);
+            Assert.Equal("200", element.Attribute("x2")?.Value);
+            Assert.Equal("250", element.Attribute("y2")?.Value);
+        }
+    }
+}
--- BlazorPlayground.Graphics/SvgFileParser.cs
+++ BlazorPlayground.Graphics/SvgFileParser.cs
@@ -1,7 +1,25 @@
-﻿namespace BlazorPlayground.Graphics {
+﻿using System.Xml.Linq;
+
+namespace BlazorPlayground.Graphics {
     public static class SvgFileParser {
-        public static SvgFileParseResult Deserialize(string contents) {
-            return new SvgFileParseResult("Todo!");
+        public static SvgFileParseResult Parse(string contents) {
+            try {
+                var graphicsElement = XElement.Parse(contents);
+                var shapes = new List<Shape>();
+
+                if (graphicsElement.Name.ToString().ToLower() != "svg") {
+                    return new SvgFileParseResult("The provided file is not a valid svg file.");
+                }
+
+                foreach (var element in graphicsElement.Elements()) {
+                    shapes.Add(new RawShape(element));
+                }
+
+                return new SvgFileParseResult(shapes);
+            }
+            catch {
+                return new SvgFileParseResult("The provided file is not a valid svg file.");
+            }
         }
     }
 }
