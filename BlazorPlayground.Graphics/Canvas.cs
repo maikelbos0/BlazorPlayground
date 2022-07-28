ba89d3b340c4ebb25728cf71435a25a9fe13c2f4
Maikel Bos <maikel.bos0@gmail.com>
2022-07-28 20:42:59 +0200

    Move coordinate properties to canvas

--- BlazorPlayground.Graphics/Canvas.cs
+++ BlazorPlayground.Graphics/Canvas.cs
@@ -19,5 +19,19 @@
         public bool ShowGrid { get; set; } = false;
         public bool SnapToGrid { get; set; } = false;
         public List<Shape> Shapes { get; set; } = new List<Shape>();
+        public Point? StartPoint { get; set; }
+        public Point? SnappedStartPoint => Snap(StartPoint);
+        public Point? EndPoint { get; set; }
+        public Point? SnappedEndPoint => Snap(EndPoint);
+        public bool IsDragging => StartPoint != null && EndPoint != null;
+        public Point? Delta => StartPoint != null && EndPoint != null ? EndPoint - StartPoint : null;
+
+        private Point? Snap(Point? point) {
+            if (!SnapToGrid || point == null) {
+                return point;
+            }
+
+            return point.SnapToGrid(GridSize);
+        }
     }
 }
