1df81d262734209047d11342c7d9cfc74042f68b
Maikel Bos <maikel.bos0@gmail.com>
2022-07-31 09:51:11 +0200

    Group draw settings and bring under test

---
+++ BlazorPlayground.Graphics.Tests/DrawSettingsTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace BlazorPlayground.Graphics.Tests {
+    public class DrawSettingsTests {
+        [Fact]
+        public void MinimumStrokeWidth() {
+            var settings = new DrawSettings() {
+                StrokeWidth = 0
+            };
+
+            Assert.Equal(1, settings.StrokeWidth);
+        }
+
+        [Fact]
+        public void MinimumSides() {
+            var settings = new DrawSettings() {
+                Sides = 2
+            };
+
+            Assert.Equal(3, settings.Sides);
+        }
+    }
+}
---
+++ BlazorPlayground.Graphics/DrawSettings.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorPlayground.Graphics {
+    public class DrawSettings {
+        private int strokeWidth = 1;
+        private int sides = 3;
+
+        public PaintManager FillPaintManager { get; set; } = new PaintManager() { Mode = PaintMode.None, ColorValue = "#FFFFFF" };
+
+        public PaintManager StrokePaintManager { get; set; } = new PaintManager() { Mode = PaintMode.Color, ColorValue = "#000000" };
+
+        public Linecap StrokeLinecap { get; set; } = Linecap.Butt;
+
+        public Linejoin StrokeLinejoin { get; set; } = Linejoin.Miter;
+
+        public int StrokeWidth {
+            get => strokeWidth;
+            set => strokeWidth = Math.Max(value, 1);
+        }
+
+        public int Sides {
+            get => sides;
+            set => sides = Math.Max(value, 3);
+        }
+    }
+}
