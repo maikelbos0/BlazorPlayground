c1ada964b67931841f65a0e1220639bd2f3c507a
Maikel Bos <maikel.bos0@gmail.com>
2022-07-31 12:07:23 +0200

    Add shape definitions for drawing

---
+++ BlazorPlayground.Graphics.Tests/ShapeDefinitionTests.cs
@@ -0,0 +1,117 @@
+﻿using Xunit;
+
+namespace BlazorPlayground.Graphics.Tests {
+    public class ShapeDefinitionTests {
+        [Fact]
+        public void Line() {
+            var definition = Assert.Single(ShapeDefinition.Values, d => d.Name == "Line");
+
+            Assert.False(definition.UseFill);
+            Assert.False(definition.UseStrokeLinejoin);
+            Assert.True(definition.UseStrokeLinecap);
+            Assert.False(definition.UseSides);
+            Assert.False(definition.AutoSelect);
+
+            var shape = Assert.IsType<Line>(definition.Constructor(new Point(100, 200), new Point(150, 250), new DrawSettings()));
+
+            PointAssert.Equal(new Point(100, 200), shape.StartPoint);
+            PointAssert.Equal(new Point(150, 250), shape.EndPoint);
+        }
+
+        [Fact]
+        public void Rectangle() {
+            var definition = Assert.Single(ShapeDefinition.Values, d => d.Name == "Rectangle");
+
+            Assert.True(definition.UseFill);
+            Assert.True(definition.UseStrokeLinejoin);
+            Assert.False(definition.UseStrokeLinecap);
+            Assert.False(definition.UseSides);
+            Assert.False(definition.AutoSelect);
+
+            var shape = Assert.IsType<Rectangle>(definition.Constructor(new Point(100, 200), new Point(150, 250), new DrawSettings()));
+
+            PointAssert.Equal(new Point(100, 200), shape.StartPoint);
+            PointAssert.Equal(new Point(150, 250), shape.EndPoint);
+        }
+
+        [Fact]
+        public void Circle() {
+            var definition = Assert.Single(ShapeDefinition.Values, d => d.Name == "Circle");
+
+            Assert.True(definition.UseFill);
+            Assert.False(definition.UseStrokeLinejoin);
+            Assert.False(definition.UseStrokeLinecap);
+            Assert.False(definition.UseSides);
+            Assert.False(definition.AutoSelect);
+
+            var shape = Assert.IsType<Circle>(definition.Constructor(new Point(100, 200), new Point(150, 250), new DrawSettings()));
+
+            PointAssert.Equal(new Point(100, 200), shape.CenterPoint);
+            PointAssert.Equal(new Point(150, 250), shape.RadiusPoint);
+        }
+
+        [Fact]
+        public void Ellipse() {
+            var definition = Assert.Single(ShapeDefinition.Values, d => d.Name == "Ellipse");
+
+            Assert.True(definition.UseFill);
+            Assert.False(definition.UseStrokeLinejoin);
+            Assert.False(definition.UseStrokeLinecap);
+            Assert.False(definition.UseSides);
+            Assert.False(definition.AutoSelect);
+
+            var shape = Assert.IsType<Ellipse>(definition.Constructor(new Point(100, 200), new Point(150, 250), new DrawSettings()));
+
+            PointAssert.Equal(new Point(100, 200), shape.CenterPoint);
+            PointAssert.Equal(new Point(150, 250), shape.RadiusPoint);
+        }
+
+        [Fact]
+        public void RegularPolygon() {
+            var definition = Assert.Single(ShapeDefinition.Values, d => d.Name == "Regular polygon");
+
+            Assert.True(definition.UseFill);
+            Assert.True(definition.UseStrokeLinejoin);
+            Assert.False(definition.UseStrokeLinecap);
+            Assert.True(definition.UseSides);
+            Assert.False(definition.AutoSelect);
+
+            var shape = Assert.IsType<RegularPolygon>(definition.Constructor(new Point(100, 200), new Point(150, 250), new DrawSettings()));
+
+            PointAssert.Equal(new Point(100, 200), shape.CenterPoint);
+            PointAssert.Equal(new Point(150, 250), shape.RadiusPoint);
+        }
+
+        [Fact]
+        public void QuadraticBezier() {
+            var definition = Assert.Single(ShapeDefinition.Values, d => d.Name == "Quadratic bezier");
+
+            Assert.True(definition.UseFill);
+            Assert.False(definition.UseStrokeLinejoin);
+            Assert.True(definition.UseStrokeLinecap);
+            Assert.False(definition.UseSides);
+            Assert.True(definition.AutoSelect);
+
+            var shape = Assert.IsType<QuadraticBezier>(definition.Constructor(new Point(100, 200), new Point(150, 250), new DrawSettings()));
+
+            PointAssert.Equal(new Point(100, 200), shape.StartPoint);
+            PointAssert.Equal(new Point(150, 250), shape.EndPoint);
+        }
+
+        [Fact]
+        public void CubicBezier() {
+            var definition = Assert.Single(ShapeDefinition.Values, d => d.Name == "Cubic bezier");
+
+            Assert.True(definition.UseFill);
+            Assert.False(definition.UseStrokeLinejoin);
+            Assert.True(definition.UseStrokeLinecap);
+            Assert.False(definition.UseSides);
+            Assert.True(definition.AutoSelect);
+
+            var shape = Assert.IsType<CubicBezier>(definition.Constructor(new Point(100, 200), new Point(150, 250), new DrawSettings()));
+
+            PointAssert.Equal(new Point(100, 200), shape.StartPoint);
+            PointAssert.Equal(new Point(150, 250), shape.EndPoint);
+        }
+    }
+}
---
+++ BlazorPlayground.Graphics/ShapeDefinition.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+
+namespace BlazorPlayground.Graphics {
+    public class ShapeDefinition {
+        public delegate Shape Construct(Point startPoint, Point endPoint, DrawSettings settings);
+
+        public static IEnumerable<ShapeDefinition> Values = new ReadOnlyCollection<ShapeDefinition>(new List<ShapeDefinition>() {
+            new("Line", (startPoint, endPoint, settings) => new Line(startPoint, endPoint), useStrokeLinecap: true),
+            new("Rectangle", (startPoint, endPoint, settings) => new Rectangle(startPoint, endPoint), useFill: true, useStrokeLinejoin: true),
+            new("Circle", (startPoint, endPoint, settings) => new Circle(startPoint, endPoint), useFill: true),
+            new("Ellipse", (startPoint, endPoint, settings) => new Ellipse(startPoint, endPoint), useFill: true),
+            new("Regular polygon", (startPoint, endPoint, settings) => new RegularPolygon(startPoint, endPoint, settings.Sides), useFill: true, useStrokeLinejoin: true, useSides: true),
+            new("Quadratic bezier", (startPoint, endPoint, settings) => new QuadraticBezier(startPoint, endPoint), useFill: true, useStrokeLinecap: true, autoSelect: true),
+            new("Cubic bezier", (startPoint, endPoint, settings) => new CubicBezier(startPoint, endPoint), useFill: true, useStrokeLinecap: true, autoSelect: true)
+        });
+
+        public string Name { get; }
+        public Construct Constructor { get; }
+        public bool UseFill { get; }
+        public bool UseStrokeLinecap { get; }
+        public bool UseStrokeLinejoin { get; }
+        public bool UseSides { get; }
+        public bool AutoSelect { get; }
+
+        private ShapeDefinition(string name, Construct constructor, bool useFill = false, bool useStrokeLinecap = false, bool useStrokeLinejoin = false, bool useSides = false, bool autoSelect = false) {
+            Name = name;
+            Constructor = constructor;
+            UseFill = useFill;
+            UseStrokeLinecap = useStrokeLinecap;
+            UseStrokeLinejoin = useStrokeLinejoin;
+            UseSides = useSides;
+            AutoSelect = autoSelect;
+        }
+    }
+}
