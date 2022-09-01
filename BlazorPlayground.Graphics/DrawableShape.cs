326243c5aa14d595dc16956240a79ea53f499dcf
Maikel Bos <maikel.bos0@gmail.com>
2022-09-01 20:23:09 +0200

    Add opacity rendering/export to drawable shape

--- BlazorPlayground.Graphics/DrawableShape.cs
+++ BlazorPlayground.Graphics/DrawableShape.cs
@@ -1,58 +1,77 @@
 ﻿using Microsoft.AspNetCore.Components.Rendering;
+using System.Globalization;
 using System.Xml.Linq;
 
 namespace BlazorPlayground.Graphics {
     public abstract class DrawableShape : Shape {
         public abstract string ElementName { get; }
 
-        public override void BuildRenderTree(RenderTreeBuilder builder) {            
+        public override void BuildRenderTree(RenderTreeBuilder builder) {
             builder.OpenElement(1, ElementName);
 
-            if (Definition.UseFill) {
-                builder.AddAttribute(2, "fill", Fill);
+            if (Definition.UseOpacity) {
+                builder.AddAttribute(2, "opacity", Opacity.ToString(CultureInfo.InvariantCulture));
             }
 
-            builder.AddAttribute(3, "stroke", Stroke);
-            builder.AddAttribute(4, "stroke-width", StrokeWidth);
+            if (Definition.UseFill) {
+                builder.AddAttribute(3, "fill", Fill);
+            }
+
+            if (Definition.UseStroke) {
+                builder.AddAttribute(4, "stroke", Stroke);
+            }
+
+            if (Definition.UseStrokeWidth) {
+                builder.AddAttribute(5, "stroke-width", StrokeWidth);
+            }
 
             if (Definition.UseStrokeLinecap) {
-                builder.AddAttribute(5, "stroke-linecap", StrokeLinecap.ToString().ToLower());
+                builder.AddAttribute(6, "stroke-linecap", StrokeLinecap.ToString().ToLower());
             }
 
             if (Definition.UseStrokeLinejoin) {
-                builder.AddAttribute(6, "stroke-linejoin", StrokeLinejoin.ToString().ToLower());
+                builder.AddAttribute(7, "stroke-linejoin", StrokeLinejoin.ToString().ToLower());
             }
 
-            builder.AddMultipleAttributes(7, GetAttributes());
+            builder.AddMultipleAttributes(8, GetAttributes());
             builder.CloseElement();
 
-            builder.OpenElement(8, ElementName);
-            builder.AddAttribute(9, "class", "shape-selector");
+            builder.OpenElement(9, ElementName);
+            builder.AddAttribute(10, "class", "shape-selector");
 
             if (Definition.UseStrokeLinecap) {
-                builder.AddAttribute(10, "stroke-linecap", StrokeLinecap.ToString().ToLower());
+                builder.AddAttribute(11, "stroke-linecap", StrokeLinecap.ToString().ToLower());
             }
 
             if (Definition.UseStrokeLinejoin) {
-                builder.AddAttribute(11, "stroke-linejoin", StrokeLinejoin.ToString().ToLower());
+                builder.AddAttribute(12, "stroke-linejoin", StrokeLinejoin.ToString().ToLower());
             }
 
-            builder.AddAttribute(12, "stroke-width", Math.Max(StrokeWidth, 12));
-            builder.AddMultipleAttributes(13, GetAttributes());
+            builder.AddAttribute(13, "stroke-width", Math.Max(StrokeWidth, 12));
+            builder.AddMultipleAttributes(14, GetAttributes());
             builder.CloseElement();
         }
 
         public abstract ShapeAttributeCollection GetAttributes();
-        
+
         public override XElement CreateSvgElement() => new(ElementName, CreateSvgAttributes());
 
         private IEnumerable<XAttribute> CreateSvgAttributes() {
+            if (Definition.UseOpacity) {
+                yield return new XAttribute("opacity", Opacity);
+            }
+
             if (Definition.UseFill) {
                 yield return new XAttribute("fill", Fill);
             }
 
-            yield return new XAttribute("stroke", Stroke);
-            yield return new XAttribute("stroke-width", StrokeWidth);
+            if (Definition.UseStroke) {
+                yield return new XAttribute("stroke", Stroke);
+            }
+
+            if (Definition.UseStrokeWidth) {
+                yield return new XAttribute("stroke-width", StrokeWidth);
+            }
 
             if (Definition.UseStrokeLinecap) {
                 yield return new XAttribute("stroke-linecap", StrokeLinecap.ToString().ToLower());
