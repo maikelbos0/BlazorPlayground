9efaab83bb9ce23f4f02a708ad46d8be7e557e8b
Maikel Bos <maikel.bos0@gmail.com>
2022-09-01 20:30:31 +0200

    Add opacity to shape definition

--- BlazorPlayground.Graphics/ShapeDefinition.cs
+++ BlazorPlayground.Graphics/ShapeDefinition.cs
@@ -3,13 +3,13 @@
         public delegate Shape Constructor(Point startPoint, Point endPoint);
 
         private readonly static Dictionary<Type, ShapeDefinition> definitions = new() {
-            { typeof(Line), new("Line", (startPoint, endPoint) => new Line(startPoint, endPoint), useStroke: true, useStrokeWidth: true, useStrokeLinecap: true) },
-            { typeof(Rectangle), new("Rectangle", (startPoint, endPoint) => new Rectangle(startPoint, endPoint), useFill: true, useStroke: true, useStrokeWidth: true, useStrokeLinejoin: true) },
-            { typeof(Circle), new("Circle", (startPoint, endPoint) => new Circle(startPoint, endPoint), useFill: true, useStroke: true, useStrokeWidth: true) },
-            { typeof(Ellipse), new("Ellipse", (startPoint, endPoint) => new Ellipse(startPoint, endPoint), useFill: true, useStroke: true, useStrokeWidth: true) },
-            { typeof(RegularPolygon), new("Regular polygon", (startPoint, endPoint) => new RegularPolygon(startPoint, endPoint), useFill: true, useStroke: true, useStrokeWidth: true, useStrokeLinejoin: true, useSides: true) },
-            { typeof(QuadraticBezier), new("Quadratic bezier", (startPoint, endPoint) => new QuadraticBezier(startPoint, endPoint), useFill: true, useStroke: true, useStrokeWidth: true, useStrokeLinecap: true, autoSelect: true) },
-            { typeof(CubicBezier), new("Cubic bezier", (startPoint, endPoint) => new CubicBezier(startPoint, endPoint), useFill: true, useStroke: true, useStrokeWidth: true, useStrokeLinecap: true, autoSelect: true) },
+            { typeof(Line), new("Line", (startPoint, endPoint) => new Line(startPoint, endPoint), useOpacity: true, useStroke: true, useStrokeWidth: true, useStrokeLinecap: true) },
+            { typeof(Rectangle), new("Rectangle", (startPoint, endPoint) => new Rectangle(startPoint, endPoint), useOpacity: true, useFill: true, useStroke: true, useStrokeWidth: true, useStrokeLinejoin: true) },
+            { typeof(Circle), new("Circle", (startPoint, endPoint) => new Circle(startPoint, endPoint), useOpacity: true, useFill: true, useStroke: true, useStrokeWidth: true) },
+            { typeof(Ellipse), new("Ellipse", (startPoint, endPoint) => new Ellipse(startPoint, endPoint), useOpacity: true, useFill: true, useStroke: true, useStrokeWidth: true) },
+            { typeof(RegularPolygon), new("Regular polygon", (startPoint, endPoint) => new RegularPolygon(startPoint, endPoint), useOpacity: true, useFill: true, useStroke: true, useStrokeWidth: true, useStrokeLinejoin: true, useSides: true) },
+            { typeof(QuadraticBezier), new("Quadratic bezier", (startPoint, endPoint) => new QuadraticBezier(startPoint, endPoint), useOpacity: true, useFill: true, useStroke: true, useStrokeWidth: true, useStrokeLinecap: true, autoSelect: true) },
+            { typeof(CubicBezier), new("Cubic bezier", (startPoint, endPoint) => new CubicBezier(startPoint, endPoint), useOpacity: true, useFill: true, useStroke: true, useStrokeWidth: true, useStrokeLinecap: true, autoSelect: true) },
             { typeof(RawShape), new("Raw shape") }
         };
 
@@ -24,6 +24,7 @@
         public string Name { get; }
         public bool IsConstructable { get; }
         public Constructor Construct { get; }
+        public bool UseOpacity { get; }
         public bool UseFill { get; }
         public bool UseStroke { get; }
         public bool UseStrokeWidth { get; }
@@ -32,7 +33,7 @@
         public bool UseSides { get; }
         public bool AutoSelect { get; }
 
-        private ShapeDefinition(string name, Constructor? construct = null, bool useFill = false, bool useStroke = false, bool useStrokeWidth = false, bool useStrokeLinecap = false, bool useStrokeLinejoin = false, bool useSides = false, bool autoSelect = false) {
+        private ShapeDefinition(string name, Constructor? construct = null, bool useOpacity = false, bool useFill = false, bool useStroke = false, bool useStrokeWidth = false, bool useStrokeLinecap = false, bool useStrokeLinejoin = false, bool useSides = false, bool autoSelect = false) {
             Name = name;
             IsConstructable = construct != null;
             Construct = construct ?? ((_, _) => throw new InvalidOperationException($"{nameof(Construct)} can only be called when {nameof(IsConstructable)} is true."));
