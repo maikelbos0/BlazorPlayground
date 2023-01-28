6f4f784fdf4211b724284e8c7b16908fe16f217c
Maikel Bos <maikel.bos0@gmail.com>
2023-01-28 16:54:33 +0100

    First chart class setup

---
+++ BlazorPlayground.Chart/DataSeries.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorPlayground.Chart;
+
+public class DataSeries : List<double?> {
+    public string Name { get; set; }
+
+    public DataSeries(string name) {
+        Name = name;
+    }
+}
