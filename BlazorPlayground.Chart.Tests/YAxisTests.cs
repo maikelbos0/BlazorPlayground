712d574c9ab423974f54000585dbb4a4ce05a86a
Maikel Bos <maikel.bos0@gmail.com>
2023-01-28 17:19:35 +0100

    Add y axis as a concept

---
+++ BlazorPlayground.Chart.Tests/YAxisTests.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace BlazorPlayground.Chart.Tests;
+
+public class YAxisTests {
+    [Fact]
+    public void GridLines() {
+        var subject = new YAxis(20, -10, 105);
+
+        Assert.Equal(new double[] { 0, 20, 40, 60, 80, 100 }, subject.GetGridLines());
+    }
+}
---
+++ BlazorPlayground.Chart/YAxis.cs
@@ -0,0 +1,27 @@
+﻿namespace BlazorPlayground.Chart;
+
+public class YAxis {
+    public YAxis AutoScale() {
+        throw new NotImplementedException();
+    }
+
+    public double Min { get; }
+    public double Max { get; }
+    public double GridLineInterval { get; }
+
+    public YAxis(double gridLineInterval, double min, double max) {
+        GridLineInterval = gridLineInterval;
+        Min = min;
+        Max = max;
+    }
+
+    public IEnumerable<double> GetGridLines() {
+        var gridLine = Min.CeilingToScale(GridLineInterval);
+
+        while (gridLine < Max) {
+            yield return gridLine;
+
+            gridLine += GridLineInterval;
+        }
+    }
+}
