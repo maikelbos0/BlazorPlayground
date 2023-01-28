0fdbbfc7ca797ba9acb4ba36fc5e5530ec27796a
Maikel Bos <maikel.bos0@gmail.com>
2023-01-28 17:43:05 +0100

    Auto scaling of axis

--- BlazorPlayground.Chart.Tests/YAxisTests.cs
+++ BlazorPlayground.Chart.Tests/YAxisTests.cs
@@ -3,6 +3,19 @@
 namespace BlazorPlayground.Chart.Tests;
 
 public class YAxisTests {
+
+    [Theory]
+    [InlineData(1.0, 0.0, 5.0)]
+    [InlineData(1.0, 100.5, 105.5, 103.0)]
+    // TODO add more tests
+    public void AutoScale(double expectedGridLineInterval, double expectedMin, double expectedMax, params double[] dataPoints) {
+        var result = YAxis.AutoScale(dataPoints);
+
+        Assert.Equal(expectedGridLineInterval, result.GridLineInterval);
+        Assert.Equal(expectedMin, result.Min);
+        Assert.Equal(expectedMax, result.Max);
+    }
+
     [Fact]
     public void GridLines() {
         var subject = new YAxis(20, -10, 105);
--- BlazorPlayground.Chart/YAxis.cs
+++ BlazorPlayground.Chart/YAxis.cs
@@ -1,8 +1,29 @@
 ﻿namespace BlazorPlayground.Chart;
 
 public class YAxis {
-    public YAxis AutoScale() {
-        throw new NotImplementedException();
+    public static YAxis AutoScale(IEnumerable<double> dataPoints) {
+        const double defaultMin = 0;
+        const double defaultMax = 5;
+        const double defaultMiddle = (defaultMax - defaultMin) / 2;
+        const int maxGridLineCount = 8;
+        double[] gridLineMultipliers = new[] { 2, 2.5, 2, 2 };
+
+        var min = dataPoints.DefaultIfEmpty(defaultMin).Min();
+        var max = dataPoints.DefaultIfEmpty(defaultMax).Max();
+
+        if (min == max) {
+            min -= defaultMiddle;
+            max += defaultMiddle;
+        }
+
+        var gridLineInterval = Math.Pow(10, Math.Floor(Math.Log10(max - min) - 1));
+        var i = 0;
+
+        while ((max.CeilingToScale(gridLineInterval) - min.FloorToScale(gridLineInterval)) / gridLineInterval > maxGridLineCount) {
+            gridLineInterval *= gridLineMultipliers[i++];
+        }
+
+        return new YAxis(gridLineInterval, min, max);
     }
 
     public double Min { get; }
