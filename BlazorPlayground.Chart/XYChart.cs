5277f39dbcae3161811d273ed68effdab4e8485e
Maikel Bos <maikel.bos0@gmail.com>
2023-06-18 19:12:53 +0200

    Move canvas size to chart

--- BlazorPlayground.Chart/XYChart.cs
+++ BlazorPlayground.Chart/XYChart.cs
@@ -5,5 +5,8 @@ public class XYChart {
     public List<DataSeries> DataSeries { get; set; } = new();
     public YAxis YAxis { get; set; } = new();
 
+    public int CanvasWidth { get; set; } = 1200;
+    public int CanvasHeight { get; set; } = 600;
+
     public void AutoScale() => YAxis.AutoScale(DataSeries.SelectMany(dataSeries => dataSeries.Where(dataPoint => dataPoint != null).Select(dataPoint => dataPoint!.Value)));
 }
