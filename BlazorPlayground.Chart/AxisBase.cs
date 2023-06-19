3a3c8069bcd58dc164cf9f153d8c298e0c1d25fd
Maikel Bos <maikel.bos0@gmail.com>
2023-06-19 13:05:39 +0200

    Add axis dimensions

--- BlazorPlayground.Chart/AxisBase.cs
+++ BlazorPlayground.Chart/AxisBase.cs
@@ -1,5 +1,6 @@
 ﻿namespace BlazorPlayground.Chart;
 
 public class AxisBase {
-    public int Size { get; set; } = 80;
+    public int Size { get; set; } = 100;
+    public int LabelClearance { get; set; } = 10;
 }
