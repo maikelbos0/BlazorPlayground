0b661583c5fb21b2b2b3b24ef6e59c7c43d93863
Maikel Bos <maikel.bos0@gmail.com>
2023-06-21 18:41:18 +0200

    Add grid line class

---
+++ BlazorPlayground.Chart.Tests/GridLineTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace BlazorPlayground.Chart.Tests;
+
+public class GridLineTests {
+    [Fact]
+    public void GetAttributes() {
+        var subject = new GridLine(20, 50, 80);
+
+        var result = subject.GetAttributes();
+
+        Assert.Equal(4, result.Count());
+        Assert.Equal("20", Assert.Single(result, attribute => attribute.Key == "x1").Value);
+        Assert.Equal("50", Assert.Single(result, attribute => attribute.Key == "y1").Value);
+        Assert.Equal("100", Assert.Single(result, attribute => attribute.Key == "x2").Value);
+        Assert.Equal("50", Assert.Single(result, attribute => attribute.Key == "y2").Value);
+    }
+}
---
+++ BlazorPlayground.Chart/GridLine.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorPlayground.Chart;
+
+public class GridLine : Shape {
+    public override string CssClass => "grid-line";
+    public override string ElementName => "line";
+    public int X { get; }
+    public int Y { get; }
+    public int Width { get; }
+
+    public GridLine(int x, int y, int width) {
+        X = x;
+        Y = y;
+        Width = width;
+    }
+
+    public override ShapeAttributeCollection GetAttributes() => new() {
+        { "x1", X },
+        { "y1", Y },
+        { "x2", X + Width },
+        { "y2", Y }
+    };           
+}
