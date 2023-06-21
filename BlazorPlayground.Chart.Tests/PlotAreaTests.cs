0f2b34afd5ffa4cdd5625eb8430db5eb5fda539c
Maikel Bos <maikel.bos0@gmail.com>
2023-06-21 21:03:12 +0200

    Add plot area tests

---
+++ BlazorPlayground.Chart.Tests/PlotAreaTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace BlazorPlayground.Chart.Tests;
+
+public class PlotAreaTests {
+    [Fact]
+    public void GetAttributes() {
+        var subject = new PlotArea(20, 50, 80, 90);
+
+        var result = subject.GetAttributes();
+
+        Assert.Equal(4, result.Count());
+        Assert.Equal("20", Assert.Single(result, attribute => attribute.Key == "x").Value);
+        Assert.Equal("50", Assert.Single(result, attribute => attribute.Key == "y").Value);
+        Assert.Equal("80", Assert.Single(result, attribute => attribute.Key == "width").Value);
+        Assert.Equal("90", Assert.Single(result, attribute => attribute.Key == "height").Value);
+    }
+}
