14b2d119af728fcd7b4cf3668f9bbbf247437c76
Maikel Bos <maikel.bos0@gmail.com>
2023-06-24 17:12:17 +0200

    Add default colors to data series

---
+++ BlazorPlayground.Chart.Tests/DataSeriesTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace BlazorPlayground.Chart.Tests;
+
+public class DataSeriesTests {
+    [Theory]
+    [InlineData(0, "red")]
+    [InlineData(1, "blue")]
+    [InlineData(2, "green")]
+    [InlineData(3, "red")]
+    public void GetColor(int index, string expectedColor) {
+        DataSeries.DefaultColors = new List<string>() { "red", "blue", "green" };
+
+        Assert.Equal(expectedColor, DataSeries.GetColor(index));
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(-1, "red", "red", "blue", "green")]
+    public void GetColor_Fallback(int index, params string[] defaultColors) {
+        DataSeries.DefaultColors = defaultColors.ToList();
+
+        Assert.Equal(DataSeries.FallbackColor, DataSeries.GetColor(index));
+    }
+}
