11e68ef011a401666fff7541ca98b40974359666
Maikel Bos <maikel.bos0@gmail.com>
2023-06-27 21:20:15 +0200

    I've got the power

---
+++ BlazorPlayground.Chart.Tests/DecimalMathTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace BlazorPlayground.Chart.Tests;
+
+public class DecimalMathTests {
+    [Theory]
+    [MemberData(nameof(PowData))]
+    public void Pow(decimal x, int y, decimal expectedResult) {
+        Assert.Equal(expectedResult, DecimalMath.Pow(x, y));
+    }
+
+    public static TheoryData<decimal, int, decimal> PowData() => new() {
+        { 10M, -5, 0.000_01M },
+        { 10M, -2, 0.01M },
+        { 10M, -1, 0.1M },
+        { 10M, 0, 1M },
+        { 10M, 1, 10M },
+        { 10M, 2, 100M },
+        { 10M, 5, 100_000M },
+        { 2.5M, 2, 6.25M },
+        { 2.5M, -2, 0.16M }
+    };
+}
---
+++ BlazorPlayground.Chart/DecimalMath.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorPlayground.Chart;
+
+public static class DecimalMath {
+    public static decimal Pow(decimal x, int y) {
+        var result = 1M;
+        
+        if (y < 0) {
+            y *= -1;
+            x = 1 / x;
+        }
+        
+        while (y > 0) {
+            if ((y & 1) != 0) {
+                result *= x;
+            }
+            y >>= 1;
+            x *= x;
+        }
+
+        return result;
+    }
+}
