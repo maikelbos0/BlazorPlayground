e102628aa2f3e508cf96d6f88aa342a7fe56eb4b
Maikel Bos <maikel.bos0@gmail.com>
2023-07-01 17:44:43 +0200

    Add basic bar shape

---
+++ BlazorPlayground.Chart.Tests/Shapes/BarDataShapeTests.cs
@@ -0,0 +1,21 @@
+﻿using BlazorPlayground.Chart.Shapes;
+using System.Linq;
+using Xunit;
+
+namespace BlazorPlayground.Chart.Tests.Shapes;
+
+public class BarDataShapeTests {
+    [Fact]
+    public void GetAttributes() {
+        var subject = new BarDataShape(20, 50, 80, 90, "red");
+
+        var result = subject.GetAttributes();
+
+        Assert.Equal(5, result.Count());
+        Assert.Equal("20", Assert.Single(result, attribute => attribute.Key == "x").Value);
+        Assert.Equal("50", Assert.Single(result, attribute => attribute.Key == "y").Value);
+        Assert.Equal("80", Assert.Single(result, attribute => attribute.Key == "width").Value);
+        Assert.Equal("90", Assert.Single(result, attribute => attribute.Key == "height").Value);
+        Assert.Equal("red", Assert.Single(result, attribute => attribute.Key == "fill").Value);
+    }
+}
---
+++ BlazorPlayground.Chart/Shapes/BarDataShape.cs
@@ -0,0 +1,27 @@
+﻿namespace BlazorPlayground.Chart.Shapes;
+
+public class BarDataShape : ShapeBase {
+    public override string CssClass => "bar-data";
+    public override string ElementName => "rect";
+    public decimal X { get; }
+    public decimal Y { get; }
+    public decimal Width { get; }
+    public decimal Height { get; }
+    public string Color { get; }
+
+    public BarDataShape(decimal x, decimal y, decimal width, decimal height, string color) {
+        X = x;
+        Y = y;
+        Width = width;
+        Height = height;
+        Color = color;
+    }
+
+    public override ShapeAttributeCollection GetAttributes() => new() {
+        { "x", X },
+        { "y", Y },
+        { "width", Width },
+        { "height", Height },
+        { "fill", Color }
+    };
+}
