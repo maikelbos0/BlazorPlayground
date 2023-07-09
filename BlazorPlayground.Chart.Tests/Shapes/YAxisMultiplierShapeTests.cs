7f0d2f4cd00d211a5b7be17ca4b1ac1a38d33804
Maikel Bos <maikel.bos0@gmail.com>
2023-07-09 14:31:29 +0200

    Add multiplier text shape for y axis

---
+++ BlazorPlayground.Chart.Tests/Shapes/YAxisMultiplierShapeTests.cs
@@ -0,0 +1,32 @@
+﻿using BlazorPlayground.Chart.Shapes;
+using System.Linq;
+using Xunit;
+
+namespace BlazorPlayground.Chart.Tests.Shapes;
+
+public class YAxisMultiplierShapeTests {
+    [Fact]
+    public void GetKey() {
+        var subject = new YAxisMultiplierShape(100M, 50M, "x 1.000");
+
+        Assert.Equal("YAxisMultiplierShape/100/50/x 1.000", subject.GetKey());
+    }
+
+    [Fact]
+    public void GetAttributes() {
+        var subject = new YAxisMultiplierShape(100M, 50M, "x 1.000");
+
+        var result = subject.GetAttributes();
+
+        Assert.Equal(2, result.Count());
+        Assert.Equal("100", Assert.Single(result, attribute => attribute.Key == "x").Value);
+        Assert.Equal("50", Assert.Single(result, attribute => attribute.Key == "y").Value);
+    }
+
+    [Fact]
+    public void GetContent() {
+        var subject = new YAxisMultiplierShape(100M, 50M, "x 1.000");
+
+        Assert.Equal("x 1.000", subject.GetContent());
+    }
+}
---
+++ BlazorPlayground.Chart/Shapes/YAxisMultiplierShape.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorPlayground.Chart.Shapes;
+
+public class YAxisMultiplierShape : ShapeBase {
+    public override string CssClass => "y-axis-multiplier";
+    public override string ElementName => "text";
+    public decimal X { get; }
+    public decimal Y { get; }
+    public string Multiplier { get; }
+
+    public YAxisMultiplierShape(decimal x, decimal y, string multiplier) {
+        X = x;
+        Y = y;
+        Multiplier = multiplier;
+    }
+
+    public override string GetKey() => $"{nameof(YAxisMultiplierShape)}/{X}/{Y}/{Multiplier}";
+
+    public override ShapeAttributeCollection GetAttributes() => new() {
+        { "x", X },
+        { "y", Y }
+    };
+
+    public override string? GetContent() => Multiplier;
+}
