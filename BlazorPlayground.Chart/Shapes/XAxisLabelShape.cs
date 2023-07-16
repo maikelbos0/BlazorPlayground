e9ba2587142e0552bf1bc3deb0ab44ce2878da82
Maikel Bos <maikel.bos0@gmail.com>
2023-07-16 13:21:53 +0200

    Add x axis label shape

---
+++ BlazorPlayground.Chart/Shapes/XAxisLabelShape.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazorPlayground.Chart.Shapes;
+
+public class XAxisLabelShape : ShapeBase {
+
+    public override string CssClass => "x-axis-label";
+    public override string ElementName => "text";
+    public decimal X { get; }
+    public decimal Y { get; }
+    public string Label { get; }
+
+    public XAxisLabelShape(decimal x, decimal y, string label, int index) : base(index) {
+        X = x;
+        Y = y;
+        Label = label;
+    }
+
+    public override ShapeAttributeCollection GetAttributes() => new() {
+        { "x", X },
+        { "y", Y }
+    };
+
+    public override string? GetContent() => Label;
+}
