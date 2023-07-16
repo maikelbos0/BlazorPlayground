35675332afb6e70ae49ec32835273ffdbb99c6e4
Maikel Bos <maikel.bos0@gmail.com>
2023-07-16 19:35:14 +0200

    Add data series layer as a grouping of data series for formatting

---
+++ BlazorPlayground.Chart.Tests/DataSeriesLayerTests.cs
@@ -0,0 +1,77 @@
+﻿using BlazorPlayground.Chart.Shapes;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace BlazorPlayground.Chart.Tests;
+
+public class DataSeriesLayerTests {
+    public class TestDataSeriesLayer : DataSeriesLayer {
+        public TestDataSeriesLayer(XYChart chart) : base(chart) { }
+
+        public override IEnumerable<ShapeBase> GetDataSeriesShapes() => throw new System.NotImplementedException();
+    }
+
+    [Theory]
+    [InlineData(0, "red")]
+    [InlineData(1, "blue")]
+    [InlineData(2, "green")]
+    [InlineData(3, "red")]
+    public void GetColor(int index, string expectedColor) {
+        DataSeriesLayer.DefaultColors = new List<string>() { "red", "blue", "green" };
+
+        Assert.Equal(expectedColor, DataSeriesLayer.GetColor(index));
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(-1, "red", "red", "blue", "green")]
+    public void GetColor_Fallback(int index, params string[] defaultColors) {
+        DataSeriesLayer.DefaultColors = defaultColors.ToList();
+
+        Assert.Equal(DataSeriesLayer.FallbackColor, DataSeriesLayer.GetColor(index));
+    }
+
+    [Fact]
+    public void AddDataSeries_With_Color() {
+        var subject = new TestDataSeriesLayer(
+            new XYChart() {
+                Labels = {
+                    "Foo",
+                    "Bar",
+                    "Baz"
+                }
+            }
+        );
+
+        var result = subject.AddDataSeries("Data", "red");
+
+        Assert.Same(result, Assert.Single(subject.DataSeries));
+        Assert.Equal("Data", result.Name);
+        Assert.Equal("red", result.Color);
+        Assert.Equal(3, result.Count);
+    }
+
+    [Fact]
+    public void AddDataSeries_Without_Color() {
+        DataSeriesLayer.DefaultColors = new List<string>() { "red", "blue", "green" };
+
+        var subject = new TestDataSeriesLayer(
+            new XYChart() {
+                Labels = {
+                    "Foo",
+                    "Bar",
+                    "Baz"
+                }
+            }
+        );
+
+        var result = subject.AddDataSeries("Data");
+
+        Assert.Same(result, Assert.Single(subject.DataSeries));
+        Assert.Equal("Data", result.Name);
+        Assert.Equal("red", result.Color);
+        Assert.Equal(3, result.Count);
+    }
+}
---
+++ BlazorPlayground.Chart/DataSeriesLayer.cs
@@ -0,0 +1,46 @@
+﻿using BlazorPlayground.Chart.Shapes;
+
+namespace BlazorPlayground.Chart;
+
+public abstract class DataSeriesLayer {
+    public const string FallbackColor = "#000000";
+
+    public static List<string> DefaultColors { get; set; } = new() {
+        // https://coolors.co/550527-688e26-faa613-f44708-a10702
+        // "#550527", "#688e26", "#faa613", "#f44708", "#a10702"
+
+        // https://coolors.co/264653-2a9d8f-e9c46a-f4a261-e76f51
+        // "#264653", "#2a9d8f", "#e9c46a", "#f4a261", "#e76f51"
+        
+        // https://coolors.co/2274a5-f75c03-f1c40f-d90368-00cc66
+        "#2274a5", "#f75c03", "#f1c40f", "#d90368", "#00cc66"
+    };
+
+    public static string GetColor(int index) {
+        if (DefaultColors.Any() && index >= 0) {
+            return DefaultColors[index % DefaultColors.Count];
+        }
+
+        return FallbackColor;
+    }
+
+    public XYChart Chart { get; }
+    public List<DataSeries> DataSeries { get; set; } = new();
+
+    public DataSeriesLayer(XYChart chart) {
+        Chart = chart;
+    }
+
+    public DataSeries AddDataSeries(string name) => AddDataSeries(name, null);
+
+    public DataSeries AddDataSeries(string name, string? color) {
+        var dataSeries = new DataSeries(name, color ?? GetColor(DataSeries.Count));
+
+        dataSeries.AddRange(Enumerable.Range(0, Chart.Labels.Count).Select<int, decimal?>(i => null));
+        DataSeries.Add(dataSeries);
+
+        return dataSeries;
+    }
+
+    public abstract IEnumerable<ShapeBase> GetDataSeriesShapes();
+}
