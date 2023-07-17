8328f03c85efdd61dcf747a62e11644e0bbd660a
Maikel Bos <maikel.bos0@gmail.com>
2023-07-17 08:48:47 +0200

    Add data series layer for bar shapes

---
+++ BlazorPlayground.Chart.Tests/BarDataSeriesLayerTests.cs
@@ -0,0 +1,53 @@
+﻿using BlazorPlayground.Chart.Shapes;
+using Xunit;
+
+namespace BlazorPlayground.Chart.Tests;
+
+public class BarDataSeriesLayerTests {
+    [Theory]
+    [MemberData(nameof(GetDataSeriesShapesData))]
+    public void GetDataSeriesShapes(int index, decimal dataPoint, decimal expectedX, decimal expectedY, decimal expectedWidth, decimal expectedHeight) {
+        var subject = new BarDataSeriesLayer(
+            new XYChart() {
+                Canvas = {
+                    Width = 1000,
+                    Height = 500,
+                    Padding = 25,
+                    XAxisLabelHeight = 50,
+                    XAxisLabelClearance = 5,
+                    YAxisLabelWidth = 100,
+                    YAxisLabelClearance = 10
+                },
+                PlotArea = {
+                     Min = -10M,
+                     Max = 40M,
+                     GridLineInterval = 10M
+                },
+                Labels = { "Foo", "Bar", "Baz", "Quux" }
+            }
+        ) {
+            DataSeries = {
+                new("Foo", "red") { null, null, null, null, 15M }
+            }
+        };
+
+        subject.DataSeries[0][index] = dataPoint;
+
+        var result = subject.GetDataSeriesShapes();
+
+        var shape = Assert.IsType<BarDataShape>(Assert.Single(result));
+
+        Assert.Equal("red", shape.Color);
+        Assert.Equal(expectedX, shape.X);
+        Assert.Equal(expectedY, shape.Y);
+        Assert.Equal(expectedWidth, shape.Width);
+        Assert.Equal(expectedHeight, shape.Height);
+        Assert.EndsWith($"[0,{index}]", shape.Key);
+    }
+
+    public static TheoryData<int, decimal, decimal, decimal, decimal, decimal> GetDataSeriesShapesData() => new() {
+        { 0, -5M, 25M + 100M + 0.5M * 850M / 4M - 5M, 25M + 40M / 50M * 400M, 10M, 5M / 50M * 400M },
+        { 1, 5M, 25M + 100M + 1.5M * 850M / 4M - 5M, 25M + (40M - 5M) / 50M * 400M, 10M, 5M / 50M * 400M },
+        { 3, 35M, 25M + 100M + 3.5M * 850M / 4M - 5M, 25M + (40M - 35M) / 50M * 400M, 10M, 35M / 50M * 400M },
+    };
+}
---
+++ BlazorPlayground.Chart/BarDataSeriesLayer.cs
@@ -0,0 +1,39 @@
+﻿using BlazorPlayground.Chart.Shapes;
+
+namespace BlazorPlayground.Chart;
+
+public class BarDataSeriesLayer : DataSeriesLayer {
+    // TODO fix x axis to include configurable width and offset
+
+    public BarDataSeriesLayer(XYChart chart) : base(chart) { }
+
+    public override IEnumerable<ShapeBase> GetDataSeriesShapes() {
+        var zeroY = Chart.MapDataPointToCanvas(0M);
+
+        return DataSeries.SelectMany((dataSeries, dataSeriesIndex) => dataSeries
+            .Select((dataPoint, index) => (DataPoint: dataPoint, Index: index))
+            .Where(value => value.DataPoint != null && value.Index < Chart.Labels.Count)
+            .Select(value => {
+                var y = Chart.MapDataPointToCanvas(value.DataPoint!.Value);
+                decimal height;
+
+                if (y < zeroY) {
+                    height = zeroY - y;
+                }
+                else {
+                    height = y - zeroY;
+                    y = zeroY;
+                }
+
+                return new BarDataShape(
+                    Chart.MapDataIndexToCanvas(value.Index) - 5, // TODO width, position and gap
+                    y,
+                    10, // TODO width, position and gap
+                    height,
+                    dataSeries.Color,
+                    dataSeriesIndex,
+                    value.Index
+                );
+            }));
+    }
+}
