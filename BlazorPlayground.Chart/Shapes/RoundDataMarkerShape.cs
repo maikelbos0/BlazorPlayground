8fad40ee6a30258b481bee4fabd934e0d40d5e7e
Maikel Bos <maikel.bos0@gmail.com>
2023-07-24 11:29:52 +0200

    Add marker shape

---
+++ BlazorPlayground.Chart/Shapes/RoundDataMarkerShape.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorPlayground.Chart.Shapes;
+
+public class RoundDataMarkerShape : ShapeBase {
+    public override string CssClass => "data-marker data-marker-round";
+    public override string ElementName => "circle";
+    public decimal X { get; }
+    public decimal Y { get; }
+    public decimal Radius { get; }
+    public string Color { get; }
+
+    public RoundDataMarkerShape(decimal x, decimal y, decimal radius, string color, int dataSeriesIndex, int dataPointIndex) : base(dataSeriesIndex, dataPointIndex) {
+        X = x;
+        Y = y;
+        Radius = radius;
+        Color = color;
+    }
+
+    public override ShapeAttributeCollection GetAttributes() => new() {
+        { "cx", X },
+        { "cy", Y },
+        { "r", Radius },
+        { "fill", Color }   
+    };
+}
