c65bdcf44849ae708c45640b5d7c08789fa3f1a7
Maikel Bos <maikel.bos0@gmail.com>
2023-07-29 20:36:50 +0200

    Add square marker

---
+++ BlazorPlayground.Chart.Tests/Shapes/SquareDataMarkerShapeTests.cs
@@ -0,0 +1,28 @@
+﻿using BlazorPlayground.Chart.Shapes;
+using System.Linq;
+using Xunit;
+
+namespace BlazorPlaygSquare.Chart.Tests.Shapes;
+
+public class SquareDataMarkerShapeTests {
+    [Fact]
+    public void Key() {
+        var subject = new SquareDataMarkerShape(150, 50, 20, 20, "red", 2, 5);
+
+        Assert.Equal("SquareDataMarkerShape[2,5]", subject.Key);
+    }
+
+    [Fact]
+    public void GetAttributes() {
+        var subject = new SquareDataMarkerShape(150, 50, 20, 20, "red", 2, 5);
+
+        var result = subject.GetAttributes();
+
+        Assert.Equal(5, result.Count());
+        Assert.Equal("150", Assert.Single(result, attribute => attribute.Key == "x").Value);
+        Assert.Equal("50", Assert.Single(result, attribute => attribute.Key == "y").Value);
+        Assert.Equal("20", Assert.Single(result, attribute => attribute.Key == "width").Value);
+        Assert.Equal("20", Assert.Single(result, attribute => attribute.Key == "height").Value);
+        Assert.Equal("red", Assert.Single(result, attribute => attribute.Key == "fill").Value);
+    }
+}
---
+++ BlazorPlayground.Chart/Shapes/SquareDataMarkerShape.cs
@@ -0,0 +1,27 @@
+﻿namespace BlazorPlayground.Chart.Shapes;
+
+public class SquareDataMarkerShape : ShapeBase {
+    public override string CssClass => "data-marker data-marker-square";
+    public override string ElementName => "rect";
+    public decimal X { get; }
+    public decimal Y { get; }
+    public decimal Width { get; }
+    public decimal Height { get; }
+    public string Color { get; }
+
+    public SquareDataMarkerShape(decimal x, decimal y, decimal width, decimal height, string color, int dataSeriesIndex, int dataPointIndex) : base(dataSeriesIndex, dataPointIndex) {
+        X = x;
+        Y = y;
+        Width = width;
+        Height = height;
+        Color = color;
+    }
+
+    public override ShapeAttributeCollection GetAttributes() => new() {
+        { "x", X },
+        { "y", Y },
+        { "width", Width },
+        { "height", Height },
+        { "fill", Color }
+    };
+}
