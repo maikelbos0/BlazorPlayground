552e019a0e3736c285f0a0dd83bc38b973de2d22
Maikel Bos <maikel.bos0@gmail.com>
2023-07-30 17:21:44 +0200

    Add option to hide markers

--- BlazorPlayground.Chart/LineLayer.cs
+++ BlazorPlayground.Chart/LineLayer.cs
@@ -3,13 +3,12 @@
 namespace BlazorPlayground.Chart;
 
 public class LineLayer : LayerBase {
-    public static DataMarkerDelegate DefaultDataMarker { get; set; } = DefaultDataMarkers.Round;
+    public static bool DefaultShowDataMarkers { get; set; } = true;
     public static DataMarkerDelegate DefaultDataMarkerType { get; set; } = DefaultDataMarkerTypes.Round;
 
-    public DataMarkerDelegate DataMarker { get; set; } = DefaultDataMarker;
-    public static bool ShowDataMarkers { get; set; } = DefaultShowDataMarkers;
+    public DataMarkerDelegate DataMarkerType { get; set; } = DefaultDataMarkerType;
+    public bool ShowDataMarkers { get; set; } = DefaultShowDataMarkers;
 
-    // TODO setting for show marker
     // TODO setting for marker size
     // TODO setting for show line
     // TODO setting for what do do for null in line
@@ -17,13 +16,20 @@ public class LineLayer : LayerBase {
     public LineLayer(XYChart chart) : base(chart) { }
 
     // TODO add actual lines
-    public override IEnumerable<ShapeBase> GetDataSeriesShapes()
-        => GetDataSeriesPoints().Select(point => DataMarkerType(
-            point.X,
-            point.Y,
-            10M,
-            point.Color,
-            point.DataSeriesIndex,
-            point.Index
-        ));
+    public override IEnumerable<ShapeBase> GetDataSeriesShapes() {
+        var dataSeriesPoints = GetDataSeriesPoints();
+
+        if (ShowDataMarkers) {
+            foreach (var dataSeriesPoint in dataSeriesPoints) {
+                yield return DataMarkerType(
+                    dataSeriesPoint.X,
+                    dataSeriesPoint.Y,
+                    10M,
+                    dataSeriesPoint.Color,
+                    dataSeriesPoint.DataSeriesIndex,
+                    dataSeriesPoint.Index
+                );
+            }
+        }
+    }
 }
