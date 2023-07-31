d0963c71a3113daa9769252f81e5bf48ea07d78b
Maikel Bos <maikel.bos0@gmail.com>
2023-07-31 21:04:39 +0200

    Add line shapes

---
+++ BlazorPlayground.Chart.Tests/Shapes/LineDataShapeTests.cs
@@ -0,0 +1,28 @@
+﻿using BlazorPlayground.Chart.Shapes;
+using System.Linq;
+using Xunit;
+
+namespace BlazorPlayground.Chart.Tests.Shapes;
+
+public class LineDataShapeTests {
+    [Fact]
+    public void Key() {
+        var subject = new LineDataShape(20, 50, 80, 90, "red", 2, 5);
+
+        Assert.Equal("LineDataShape[2,5]", subject.Key);
+    }
+
+    [Fact]
+    public void GetAttributes() {
+        var subject = new LineDataShape(20, 50, 80, 90, "red", 2, 5);
+
+        var result = subject.GetAttributes();
+
+        Assert.Equal(5, result.Count());
+        Assert.Equal("20", Assert.Single(result, attribute => attribute.Key == "x1").Value);
+        Assert.Equal("50", Assert.Single(result, attribute => attribute.Key == "y1").Value);
+        Assert.Equal("80", Assert.Single(result, attribute => attribute.Key == "x2").Value);
+        Assert.Equal("90", Assert.Single(result, attribute => attribute.Key == "y2").Value);
+        Assert.Equal("red", Assert.Single(result, attribute => attribute.Key == "stroke").Value);
+    }
+}
---
+++ BlazorPlayground.Chart/Shapes/LineDataShape.cs
@@ -0,0 +1,27 @@
+﻿namespace BlazorPlayground.Chart.Shapes;
+
+public class LineDataShape : ShapeBase {
+    public override string CssClass => "line-data";
+    public override string ElementName => "line";
+    public decimal X1 { get; }
+    public decimal Y1 { get; }
+    public decimal X2 { get; }
+    public decimal Y2 { get; }
+    public string Color { get; }
+
+    public LineDataShape(decimal x1, decimal y1, decimal x2, decimal y2, string color, int dataSeriesIndex, int dataPointIndex) : base(dataSeriesIndex, dataPointIndex) {
+        Y1 = y1;
+        X1 = x1;
+        Y2 = y2;
+        X2 = x2;
+        Color = color;
+    }
+
+    public override ShapeAttributeCollection GetAttributes() => new() {
+        { "x1", X1 },
+        { "y1", Y1 },
+        { "x2", X2 },
+        { "y2", Y2 },
+        { "stroke", Color }
+    };
+}
