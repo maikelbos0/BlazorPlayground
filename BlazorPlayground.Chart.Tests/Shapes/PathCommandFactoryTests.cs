7fab97a1a860bd95d9d353054545a5fc02b512a1
Maikel Bos <maikel.bos0@gmail.com>
2023-08-02 20:37:38 +0200

    Use path for line data part 2: path commands

---
+++ BlazorPlayground.Chart.Tests/Shapes/PathCommandFactoryTests.cs
@@ -0,0 +1,16 @@
+﻿using BlazorPlayground.Chart.Shapes;
+using Xunit;
+
+namespace BlazorPlayground.Chart.Tests.Shapes;
+
+public class PathCommandFactoryTests {
+    [Fact]
+    public void MoveTo() {
+        Assert.Equal("M 5.5 7.5", PathCommandFactory.MoveTo(5.5M, 7.5M));
+    }
+
+    [Fact]
+    public void LineTo() {
+        Assert.Equal("L 5.5 7.5", PathCommandFactory.LineTo(5.5M, 7.5M));
+    }
+}
---
+++ BlazorPlayground.Chart/Shapes/PathCommandFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorPlayground.Chart.Shapes;
+
+public static class PathCommandFactory {
+    public static string MoveTo(decimal x, decimal y) => FormattableString.Invariant($"M {x} {y}");
+
+    public static string LineTo(decimal x, decimal y) => FormattableString.Invariant($"L {x} {y}");
+}
