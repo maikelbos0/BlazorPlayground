367197c021ec61218b388a6290ea6e2858afd774
Maikel Bos <maikel.bos0@gmail.com>
2023-08-05 16:18:27 +0200

    Copy over canvas implementation

---
+++ BlazorPlayground.Chart.Tests/Canvas2Tests.cs
@@ -0,0 +1,85 @@
+﻿using Xunit;
+
+namespace BlazorPlayground.Chart.Tests;
+
+public class Canvas2Tests {
+    [Fact]
+    public void PlotAreaX() {
+        var subject = new Canvas2() {
+            Width = 1000,
+            Height = 500,
+            Padding = 25,
+            XAxisLabelHeight = 50,
+            XAxisLabelClearance = 5,
+            YAxisLabelWidth = 75,
+            YAxisLabelClearance = 10
+        };
+
+        Assert.Equal(25 + 75, subject.PlotAreaX);
+    }
+
+    [Fact]
+    public void PlotAreaY() {
+        var subject = new Canvas2() {
+            Width = 1000,
+            Height = 500,
+            Padding = 25,
+            XAxisLabelHeight = 50,
+            XAxisLabelClearance = 5,
+            YAxisLabelWidth = 75,
+            YAxisLabelClearance = 10
+        };
+
+        Assert.Equal(25, subject.PlotAreaY);
+    }
+
+    [Fact]
+    public void PlotAreaWidth() {
+        var subject = new Canvas2() {
+            Width = 1000,
+            Height = 500,
+            Padding = 25,
+            XAxisLabelHeight = 50,
+            XAxisLabelClearance = 5,
+            YAxisLabelWidth = 75,
+            YAxisLabelClearance = 10
+        };
+
+        Assert.Equal(1000 - 25 - 25 - 75, subject.PlotAreaWidth);
+    }
+
+    [Fact]
+    public void PlotAreaHeight() {
+        var subject = new Canvas2() {
+            Width = 1000,
+            Height = 500,
+            Padding = 25,
+            XAxisLabelHeight = 50,
+            XAxisLabelClearance = 5,
+            YAxisLabelWidth = 75,
+            YAxisLabelClearance = 10
+        };
+
+        Assert.Equal(500 - 25 - 25 - 50, subject.PlotAreaHeight);
+    }
+
+    [Fact]
+    public void GetPlotAreaShape() {
+        var subject = new Canvas2() {
+            Width = 1000,
+            Height = 500,
+            Padding = 25,
+            XAxisLabelHeight = 50,
+            XAxisLabelClearance = 5,
+            YAxisLabelWidth = 75,
+            YAxisLabelClearance = 10
+        };
+
+        var result = subject.GetPlotAreaShape();
+
+        Assert.Equal(25 + 75, result.X);
+        Assert.Equal(25, result.Y);
+        Assert.Equal(1000 - 25 - 25 - 75, result.Width);
+        Assert.Equal(500 - 25 - 25 - 50, result.Height);
+    }
+}
--- BlazorPlayground.Chart/Canvas2.cs
+++ BlazorPlayground.Chart/Canvas2.cs
@@ -4,11 +4,36 @@ using Microsoft.AspNetCore.Components.Rendering;
 namespace BlazorPlayground.Chart;
 
 public class Canvas2 : ComponentBase {
+    public static int DefaultWidth { get; set; } = 1200;
+    public static int DefaultHeight { get; set; } = 600;
+    public static int DefaultPadding { get; set; } = 25;
+    public static int DefaultXAxisLabelHeight { get; set; } = 100;
+    public static int DefaultXAxisLabelClearance { get; set; } = 10;
+    public static int DefaultYAxisLabelWidth { get; set; } = 100;
+    public static int DefaultYAxisLabelClearance { get; set; } = 10;
+    public static string DefaultYAxisLabelFormat { get; set; } = "#,##0.######";
+    public static string DefaultYAxisMultiplierFormat { get; set; } = "x #,##0.######";
+
     [CascadingParameter] internal XYChart2 Chart { get; set; } = null!;
+    [Parameter] public int Width { get; set; } = DefaultWidth;
+    [Parameter] public int Height { get; set; } = DefaultHeight;
+    [Parameter] public int Padding { get; set; } = DefaultPadding;
+    [Parameter] public int XAxisLabelHeight { get; set; } = DefaultXAxisLabelHeight;
+    [Parameter] public int XAxisLabelClearance { get; set; } = DefaultXAxisLabelClearance;
+    [Parameter] public int YAxisLabelWidth { get; set; } = DefaultYAxisLabelWidth;
+    [Parameter] public int YAxisLabelClearance { get; set; } = DefaultYAxisLabelClearance;
+    [Parameter] public string YAxisLabelFormat { get; set; } = DefaultYAxisLabelFormat;
+    [Parameter] public string YAxisMultiplierFormat { get; set; } = DefaultYAxisMultiplierFormat;
+    public int PlotAreaX => Padding + YAxisLabelWidth;
+    public int PlotAreaY => Padding;
+    public int PlotAreaWidth => Width - Padding * 2 - YAxisLabelWidth;
+    public int PlotAreaHeight => Height - Padding * 2 - XAxisLabelHeight;
 
     protected override void OnInitialized() {
         Chart.Canvas = this;
     }
 
     protected override void BuildRenderTree(RenderTreeBuilder builder) => builder.AddContent(1, nameof(Canvas2));
+
+    public Shapes.PlotAreaShape GetPlotAreaShape() => new(Width, Height, PlotAreaX, PlotAreaY, PlotAreaWidth, PlotAreaHeight);
 }
