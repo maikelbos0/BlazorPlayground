e3098bf47b714f99f1398c281a5e9accfaff3110
Maikel Bos <maikel.bos0@gmail.com>
2023-08-06 10:23:30 +0200

    Add auto scale settings

---
+++ BlazorPlayground.Chart/AutoScaleSettings2.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace BlazorPlayground.Chart;
+
+public class AutoScaleSettings2 : ComponentBase {
+    public static bool DefaultIsEnabled { get; set; } = true;
+    public static int DefaultRequestedGridLineCount { get; set; } = 11;
+    public static bool DefaultIncludeZero { get; set; } = false;
+    public static decimal DefaultClearancePercentage { get; set; } = 5M;
+
+    [CascadingParameter] internal PlotArea2 PlotArea { get; set; } = null!;
+
+    [Parameter] public bool IsEnabled { get; set; } = DefaultIsEnabled;
+    [Parameter] public int RequestedGridLineCount { get; set; } = DefaultRequestedGridLineCount;
+    [Parameter] public bool IncludeZero { get; set; } = DefaultIncludeZero;
+    [Parameter] public decimal ClearancePercentage { get; set; } = DefaultClearancePercentage;
+
+    protected override void OnInitialized() {
+        PlotArea.AutoScaleSettings = this;
+    }
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder) => builder.AddContent(1, nameof(AutoScaleSettings2));
+}
--- BlazorPlayground.Chart/PlotArea2.cs
+++ BlazorPlayground.Chart/PlotArea2.cs
@@ -5,10 +5,19 @@ namespace BlazorPlayground.Chart;
 
 public class PlotArea2 : ComponentBase {
     [CascadingParameter] internal XYChart2 Chart { get; set; } = null!;
-        
+
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+
+    public AutoScaleSettings2 AutoScaleSettings { get; set; } = new();
     protected override void OnInitialized() {
         Chart.PlotArea = this;
     }
 
-    protected override void BuildRenderTree(RenderTreeBuilder builder) => builder.AddContent(1, nameof(PlotArea2));
+    protected override void BuildRenderTree(RenderTreeBuilder builder) {
+        builder.AddContent(1, nameof(PlotArea2));
+        builder.OpenComponent<CascadingValue<PlotArea2>>(1);
+        builder.AddAttribute(2, "Value", this);
+        builder.AddAttribute(3, "ChildContent", ChildContent);
+        builder.CloseComponent();
+    }
 }
