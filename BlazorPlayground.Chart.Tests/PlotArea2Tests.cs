c36ae7e3428be9fb8e4b830071dcce1c3b666409
Maikel Bos <maikel.bos0@gmail.com>
2023-08-06 10:30:53 +0200

    Copy over plot area implementation

---
+++ BlazorPlayground.Chart.Tests/PlotArea2Tests.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using Xunit;
+
+namespace BlazorPlayground.Chart.Tests;
+
+public class PlotArea2Tests {
+    [Theory]
+    [MemberData(nameof(AutoScale_Data))]
+    public void AutoScale(decimal[] dataPoints, int requestedGridLineCount, decimal expectedGridLineInterval, decimal expectedMin, decimal expectedMax) {
+        var subject = new PlotArea2() {
+            AutoScaleSettings = {
+                IsEnabled = true,
+                IncludeZero = false,
+                ClearancePercentage = 0M,
+                RequestedGridLineCount = requestedGridLineCount
+            }
+        };
+
+        subject.AutoScale(dataPoints);
+
+        Assert.Equal(expectedMin, subject.Min);
+        Assert.Equal(expectedMax, subject.Max);
+        Assert.Equal(expectedGridLineInterval, subject.GridLineInterval);
+    }
+
+    public static TheoryData<decimal[], int, decimal, decimal, decimal> AutoScale_Data() => new() {
+        { new[] { 1M, 49M }, 6, 10M, 0M, 50M }, // 6
+        { new[] { 1M, 49M }, 11, 5M, 0M, 50M }, // 11
+        { new[] { 50M, 100M }, 11, 5M, 50M, 100M }, // 11
+
+        { new[] { 0.1M, 4.9M }, 6, 1M, 0M, 5M }, // 6
+        { new[] { 0.1M, 4.9M }, 11, 0.5M, 0M, 5M }, // 11
+        { new[] { 5M, 10M }, 11, 0.5M, 5M, 10M }, // 11
+
+        { new[] { 0.001M, 0.049M }, 6, 0.01M, 0M, 0.05M }, // 6
+        { new[] { 0.001M, 0.049M }, 11, 0.005M, 0M, 0.05M }, // 11
+        { new[] { 0.05M, 0.1M }, 11, 0.005M, 0.05M, 0.1M }, // 11
+    };
+
+    [Theory]
+    [MemberData(nameof(AutoScale_No_DataPoints_Data))]
+    public void AutoScale_No_DataPoints(int requestedGridLineCount, decimal expectedGridLineInterval, decimal expectedMin, decimal expectedMax) {
+        var subject = new PlotArea2() {
+            AutoScaleSettings = {
+                IsEnabled = true,
+                IncludeZero = false,
+                ClearancePercentage = 0M,
+                RequestedGridLineCount = requestedGridLineCount
+            }
+        };
+
+        subject.AutoScale(Array.Empty<decimal>());
+
+        Assert.Equal(expectedMin, subject.Min);
+        Assert.Equal(expectedMax, subject.Max);
+        Assert.Equal(expectedGridLineInterval, subject.GridLineInterval);
+    }
+
+    public static TheoryData<int, decimal, decimal, decimal> AutoScale_No_DataPoints_Data() => new() {
+        { 1, 50M, 0M, 50M }, // 2
+        { 2, 20M, 0M, 60M }, // 4
+        { 3, 20M, 0M, 60M }, // 4
+        { 4, 10M, 0M, 50M }, // 6
+        { 7, 10M, 0M, 50M }, // 6
+        { 8, 5M, 0M, 50M }, // 11
+        { 17, 5M, 0M, 50M }, // 11
+        { 18, 2M, 0M, 50M }, // 21
+        { 25, 2M, 0M, 50M }, // 21
+    };
+
+    [Theory]
+    [MemberData(nameof(AutoScale_IncludeZero_Data))]
+    public void AutoScale_IncludeZero(decimal[] dataPoints, decimal expectedGridLineInterval, decimal expectedMin, decimal expectedMax) {
+        var subject = new PlotArea2() {
+            AutoScaleSettings = {
+                IsEnabled = true,
+                IncludeZero = true,
+                ClearancePercentage = 0M,
+                RequestedGridLineCount = 5
+            }
+        };
+
+        subject.AutoScale(dataPoints);
+
+        Assert.Equal(expectedMin, subject.Min);
+        Assert.Equal(expectedMax, subject.Max);
+        Assert.Equal(expectedGridLineInterval, subject.GridLineInterval);
+    }
+
+    public static TheoryData<decimal[], decimal, decimal, decimal> AutoScale_IncludeZero_Data() => new() {
+        { new[] { 20M, 22M }, 5M, 0M, 25M },
+        { new[] { -20M, -22M }, 5M, -25M, 0M },
+        { new[] { 22M, -22M }, 10M, -30M, 30M },
+    };
+
+    [Theory]
+    [MemberData(nameof(AutoScale_ClearancePercentage_Data))]
+    public void AutoScale_ClearancePercentage(decimal[] dataPoints, decimal expectedGridLineInterval, decimal expectedMin, decimal expectedMax) {
+        var subject = new PlotArea2() {
+            AutoScaleSettings = {
+                IsEnabled = true,
+                IncludeZero = false,
+                ClearancePercentage = 5M,
+                RequestedGridLineCount = 5
+            }
+        };
+
+        subject.AutoScale(dataPoints);
+
+        Assert.Equal(expectedMin, subject.Min);
+        Assert.Equal(expectedMax, subject.Max);
+        Assert.Equal(expectedGridLineInterval, subject.GridLineInterval);
+    }
+
+    public static TheoryData<decimal[], decimal, decimal, decimal> AutoScale_ClearancePercentage_Data() => new() {
+        { new[] { 2M, 22M }, 5M, 0M, 25M },
+        { new[] { -2M, -22M }, 5M, -25M, 0M },
+        { new[] { 22M, -22M }, 10M, -30M, 30M },
+    };
+
+    [Fact]
+    public void AutoScale_Disabled() {
+        var subject = new PlotArea2() {
+            AutoScaleSettings = {
+                IsEnabled = false, 
+                IncludeZero = false, 
+                ClearancePercentage = 0M, 
+                RequestedGridLineCount = 9 
+            }
+        };
+
+        subject.AutoScale(new[] { 0.006M, 0.044M });
+
+        Assert.Equal(PlotArea2.DefaultMin, subject.Min);
+        Assert.Equal(PlotArea2.DefaultMax, subject.Max);
+        Assert.Equal(PlotArea2.DefaultGridLineInterval, subject.GridLineInterval);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetGridLineDataPoints_Data))]
+    public void GetGridLineDataPoints(decimal min, decimal max, decimal gridLineInterval, params decimal[] expectedDataPoints) {
+        var subject = new PlotArea2() {
+            Min = min,
+            Max = max,
+            GridLineInterval = gridLineInterval
+        };
+
+        Assert.Equal(expectedDataPoints, subject.GetGridLineDataPoints());
+    }
+
+    public static TheoryData<decimal, decimal, decimal, decimal[]> GetGridLineDataPoints_Data() => new() {
+        { 0M, 100M, 20M,  new decimal[] { 0M, 20M, 40M, 60M, 80M, 100M } },
+        { -10M, 105M, 20M,  new decimal[] { 0M, 20M, 40M, 60M, 80M, 100.0M } },
+        { -1M, 0.2M, 0.2M, new decimal[] { -1M, -0.8M, -0.6M, -0.4M, -0.2M, 0M, 0.2M } }
+    };
+}
--- BlazorPlayground.Chart/PlotArea2.cs
+++ BlazorPlayground.Chart/PlotArea2.cs
@@ -4,11 +4,22 @@ using Microsoft.AspNetCore.Components.Rendering;
 namespace BlazorPlayground.Chart;
 
 public class PlotArea2 : ComponentBase {
+    public static decimal DefaultMin { get; set; } = 0M;
+    public static decimal DefaultMax { get; set; } = 50M;
+    public static decimal DefaultGridLineInterval { get; set; } = 5M;
+    public static decimal DefaultMultiplier { get; set; } = 1M;
+
     [CascadingParameter] internal XYChart2 Chart { get; set; } = null!;
 
     [Parameter] public RenderFragment? ChildContent { get; set; }
 
     public AutoScaleSettings2 AutoScaleSettings { get; set; } = new();
+
+    public decimal Min { get; set; } = DefaultMin;
+    public decimal Max { get; set; } = DefaultMax;
+    public decimal GridLineInterval { get; set; } = DefaultGridLineInterval;
+    public decimal Multiplier { get; set; } = DefaultMultiplier;
+
     protected override void OnInitialized() {
         Chart.PlotArea = this;
     }
@@ -19,5 +30,64 @@ public class PlotArea2 : ComponentBase {
         builder.AddAttribute(2, "Value", this);
         builder.AddAttribute(3, "ChildContent", ChildContent);
         builder.CloseComponent();
+    }
+
+    public void AutoScale(IEnumerable<decimal> dataPoints) {
+        if (!AutoScaleSettings.IsEnabled) {
+            return;
+        }
+
+        var min = DefaultMin;
+        var max = DefaultMax;
+
+        if (dataPoints.Any()) {
+            min = dataPoints.Min();
+            max = dataPoints.Max();
+
+            if (min == max) {
+                min -= (DefaultMax - DefaultMin) / 2M;
+                max += (DefaultMax - DefaultMin) / 2M;
+            }
+            else {
+                var clearance = (max - min) / (100M - AutoScaleSettings.ClearancePercentage * 2) * AutoScaleSettings.ClearancePercentage;
+
+                min -= clearance;
+                max += clearance;
+            }
+
+            if (min > 0M && AutoScaleSettings.IncludeZero) {
+                min = 0M;
+            }
+
+            if (max < 0M && AutoScaleSettings.IncludeZero) {
+                max = 0M;
+            }
+        }
+
+        var rawGridLineInterval = (max - min) / Math.Max(1, AutoScaleSettings.RequestedGridLineCount - 1);
+        var baseMultiplier = DecimalMath.Pow(10M, (int)Math.Floor((decimal)Math.Log10((double)rawGridLineInterval)));
+        var scale = new[] { 1M, 2M, 5M, 10M }
+            .Select(baseGridLineInterval => baseGridLineInterval * baseMultiplier)
+            .Select(gridLineInterval => new {
+                GridLineInterval = gridLineInterval,
+                Min = DecimalMath.FloorToScale(min, gridLineInterval),
+                Max = DecimalMath.CeilingToScale(max, gridLineInterval)
+            })
+            .OrderBy(candidate => Math.Abs((candidate.Max - candidate.Min) / candidate.GridLineInterval - AutoScaleSettings.RequestedGridLineCount))
+            .First();
+
+        Min = DecimalMath.Trim(scale.Min);
+        Max = DecimalMath.Trim(scale.Max);
+        GridLineInterval = DecimalMath.Trim(scale.GridLineInterval);
+    }
+
+    public IEnumerable<decimal> GetGridLineDataPoints() {
+        var dataPoint = DecimalMath.CeilingToScale(Min, GridLineInterval);
+
+        while (dataPoint <= Max) {
+            yield return dataPoint;
+
+            dataPoint += GridLineInterval;
+        }
     }
 }
