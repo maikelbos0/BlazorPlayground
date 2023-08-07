783d1448dabff2a9abc0a4206ad6ae70b3289053
Maikel Bos <maikel.bos0@gmail.com>
2023-08-07 12:44:56 +0200

    Copy and correct shape generation

--- BlazorPlayground.Chart.Tests/XYChart2Tests.cs
+++ BlazorPlayground.Chart.Tests/XYChart2Tests.cs
@@ -1,4 +1,6 @@
-﻿using Xunit;
+﻿using BlazorPlayground.Chart.Shapes;
+using System.Linq;
+using Xunit;
 
 namespace BlazorPlayground.Chart.Tests;
 
@@ -21,6 +23,431 @@ public class XYChart2Tests {
         Assert.Equal(870M / 3M, subject.DataPointWidth);
     }
 
+
+
+    [Fact]
+    public void GetShapes_AutoScale() {
+        var subject = new XYChart2() {
+            PlotArea = {
+                Min = -4M,
+                Max = 10M,
+                GridLineInterval = 1M,
+                AutoScaleSettings = {
+                    IsEnabled = true,
+                    ClearancePercentage = 0M
+                }
+            },
+            Canvas = {
+                Height = 800,
+                Padding = 25,
+                XAxisLabelHeight = 50
+            },
+            Labels = { "Foo", "Bar", "Baz" },
+        };
+
+        subject.Layers.Add(new BarLayer2() {
+            Chart = subject,
+            IsStacked = false,
+            DataSeries = {
+                new() {
+                    Color = "blue",
+                    DataPoints = { -9M, 0M }
+                },
+                new() {
+                    Color = "red",
+                    DataPoints = { -5M, 19M }
+                }
+            }
+        });
+
+        _ = subject.GetShapes().ToList();
+
+        Assert.Equal(-10M, subject.PlotArea.Min);
+        Assert.Equal(20M, subject.PlotArea.Max);
+        Assert.Equal(2M, subject.PlotArea.GridLineInterval);
+    }
+
+    [Fact]
+    public void GetShapes_No_AutoScale() {
+        var subject = new XYChart2() {
+            PlotArea = {
+                Min = -4M,
+                Max = 10M,
+                GridLineInterval = 1M,
+                AutoScaleSettings = {
+                    IsEnabled = false
+                }
+            },
+            Canvas = {
+                Height = 800,
+                Padding = 25,
+                XAxisLabelHeight = 50
+            },
+            Labels = { "Foo", "Bar", "Baz" }
+        };
+
+        subject.Layers.Add(new BarLayer2() {
+            Chart = subject,
+            IsStacked = false,
+            DataSeries = {
+                new() {
+                    Color = "blue",
+                    DataPoints = { -9M, 0M }
+                },
+                new() {
+                    Color = "red",
+                    DataPoints = { -5M, 19M }
+                }
+            }
+        });
+
+        _ = subject.GetShapes().ToList();
+
+        Assert.Equal(-4M, subject.PlotArea.Min);
+        Assert.Equal(10M, subject.PlotArea.Max);
+        Assert.Equal(1M, subject.PlotArea.GridLineInterval);
+    }
+    
+    [Fact]
+    public void GetShapes_PlotAreaShape() {
+        var subject = new XYChart2();
+
+        Assert.Single(subject.GetShapes(), shape => shape is PlotAreaShape);
+    }
+
+    [Fact]
+    public void GetShapes_GridLineShapes() {
+        var subject = new XYChart2();
+
+        Assert.Contains(subject.GetShapes(), shape => shape is GridLineShape);
+    }
+
+    [Fact]
+    public void GetShapes_YAxisLabelShapes() {
+        var subject = new XYChart2();
+
+        Assert.Contains(subject.GetShapes(), shape => shape is YAxisLabelShape);
+    }
+
+    [Fact]
+    public void GetShapes_YAxisMultiplierShape() {
+        var subject = new XYChart2() {
+            PlotArea = {
+                Multiplier = 1000
+            }
+        };
+
+        Assert.Single(subject.GetShapes(), shape => shape is YAxisMultiplierShape);
+    }
+
+    [Fact]
+    public void GetShapes_YAxisMultiplierShape_Without_Multiplier() {
+        var subject = new XYChart2();
+
+        Assert.DoesNotContain(subject.GetShapes(), shape => shape == null);
+    }
+
+    [Fact]
+    public void GetShapes_XAxisLabelShapes() {
+        var subject = new XYChart2() {
+            Labels = { "Foo", "Bar" }
+        };
+
+        Assert.Contains(subject.GetShapes(), shape => shape is XAxisLabelShape);
+    }
+    
+    [Fact]
+    public void GetShapes_DataSeriesShapes() {
+        var subject = new XYChart2() {
+            Labels = { "Foo", "Bar" }
+        };
+
+        subject.Layers.Add(new BarLayer2() {
+            Chart = subject,
+            DataSeries = {
+                new() {
+                    Color = "blue",
+                    DataPoints = { 5M, 10M }
+                }
+            }
+        });
+
+        Assert.Contains(subject.GetShapes(), shape => shape is DataBarShape);
+    }
+    
+    [Fact]
+    public void GetScaleDataPoints_Unstacked() {
+        var subject = new XYChart2() {
+            Labels = { "Foo", "Bar", "Baz", "Quux" }
+        };
+
+        subject.Layers.Add(new BarLayer2() {
+            Chart = subject,
+            IsStacked = false,
+            DataSeries = {
+                new() {
+                    Color = "red",
+                    DataPoints = { -5M, -3M, null, null }
+                },
+                new() {
+                    Color = "green",
+                    DataPoints = { -7M, -3M, null, null }
+                },
+                new() {
+                    Color = "blue",
+                    DataPoints = { 7M, null, 3M }
+                },
+                new() {
+                    Color = "pink",
+                    DataPoints = { 5M, null, 3M }
+                }
+            }
+        });
+
+        Assert.Equal(new[] { -5M, -3M, -7M, -3M, 7M, 3M, 5M, 3M }, subject.GetScaleDataPoints());
+    }
+
+    [Fact]
+    public void GetScaleDataPoints_Stacked() {
+        var subject = new XYChart2() {
+            Labels = { "Foo", "Bar", "Baz", "Quux" }
+        };
+
+        subject.Layers.Add(new BarLayer2() {
+            Chart = subject,
+            IsStacked = true,
+            DataSeries = {
+                new() {
+                    Color = "red",
+                    DataPoints = { -5M, -3M, null, null }
+                },
+                new() {
+                    Color = "green",
+                    DataPoints = { -7M, -3M, null, null }
+                },
+                new() {
+                    Color = "blue",
+                    DataPoints = { 7M, null, 3M }
+                },
+                new() {
+                    Color = "pink",
+                    DataPoints = { 5M, null, 3M }
+                }
+            }
+        });
+
+        Assert.Equal(new[] { -5M, -3M, -7M, -3M, 7M, 3M, 5M, 3M, -12M, 12M, -6M, 6M }, subject.GetScaleDataPoints());
+    }
+    
+    [Fact]
+    public void GetGridLineShapes() {
+        var subject = new XYChart2() {
+            Canvas = {
+                Width = 1000,
+                Height = 500,
+                Padding = 25,
+                XAxisLabelHeight = 50,
+                XAxisLabelClearance = 5,
+                YAxisLabelWidth = 75,
+                YAxisLabelClearance = 10
+            },
+            PlotArea = {
+                 Min = -100M,
+                 Max = 500M,
+                 GridLineInterval = 200M
+            }
+        };
+
+        var result = subject.GetGridLineShapes();
+
+        Assert.Equal(3, result.Count());
+
+        Assert.All(result, shape => {
+            Assert.Equal(25 + 75, shape.X);
+            Assert.Equal(1000 - 25 - 25 - 75, shape.Width);
+        });
+
+        var plotAreaRange = subject.PlotArea.Max - subject.PlotArea.Min;
+
+        Assert.Single(result, shape => shape.Key.EndsWith("[0]") && shape.Y == subject.Canvas.PlotAreaY + (400M - subject.PlotArea.Min) / plotAreaRange * subject.Canvas.PlotAreaHeight);
+        Assert.Single(result, shape => shape.Key.EndsWith("[1]") && shape.Y == subject.Canvas.PlotAreaY + (200M - subject.PlotArea.Min) / plotAreaRange * subject.Canvas.PlotAreaHeight);
+        Assert.Single(result, shape => shape.Key.EndsWith("[2]") && shape.Y == subject.Canvas.PlotAreaY + (0M - subject.PlotArea.Min) / plotAreaRange * subject.Canvas.PlotAreaHeight);
+    }
+    
+    [Fact]
+    public void GetYAxisLabelShapes() {
+        var subject = new XYChart2() {
+            Canvas = {
+                Width = 1000,
+                Height = 500,
+                Padding = 25,
+                XAxisLabelHeight = 50,
+                XAxisLabelClearance = 5,
+                YAxisLabelWidth = 75,
+                YAxisLabelClearance = 10,
+                YAxisLabelFormat = "#000"
+            },
+            PlotArea = {
+                 Min = -100M,
+                 Max = 500M,
+                 GridLineInterval = 200M,
+                 Multiplier = 10M
+            }
+        };
+
+        var result = subject.GetYAxisLabelShapes();
+
+        Assert.Equal(3, result.Count());
+
+        Assert.All(result, shape => {
+            Assert.Equal(25 + 75 - 10, shape.X);
+        });
+
+        var plotAreaRange = subject.PlotArea.Max - subject.PlotArea.Min;
+
+        Assert.Single(result, shape => shape.Key.EndsWith("[0]") && shape.Y == subject.Canvas.PlotAreaY + (400M - subject.PlotArea.Min) / plotAreaRange * subject.Canvas.PlotAreaHeight && shape.Value == "000");
+        Assert.Single(result, shape => shape.Key.EndsWith("[1]") && shape.Y == subject.Canvas.PlotAreaY + (200M - subject.PlotArea.Min) / plotAreaRange * subject.Canvas.PlotAreaHeight && shape.Value == "020");
+        Assert.Single(result, shape => shape.Key.EndsWith("[2]") && shape.Y == subject.Canvas.PlotAreaY + (0M - subject.PlotArea.Min) / plotAreaRange * subject.Canvas.PlotAreaHeight && shape.Value == "040");
+    }
+
+    [Fact]
+    public void GetYAxisMultiplierShape() {
+        var subject = new XYChart2() {
+            Canvas = {
+                Width = 1000,
+                Height = 500,
+                Padding = 25,
+                XAxisLabelHeight = 50,
+                XAxisLabelClearance = 5,
+                YAxisLabelWidth = 75,
+                YAxisLabelClearance = 10,
+                YAxisMultiplierFormat = "x 0000"
+            },
+            PlotArea = {
+                 Min = -100M,
+                 Max = 500M,
+                 GridLineInterval = 200M,
+                 Multiplier = 1000
+            }
+        };
+
+        var result = subject.GetYAxisMultiplierShape();
+
+        Assert.NotNull(result);
+        Assert.Equal(25, result.X);
+        Assert.Equal(25 + (500 - 25 - 25 - 50) / 2, result.Y);
+        Assert.Equal("x 1000", result.Multiplier);
+    }
+
+    [Fact]
+    public void GetYAxisMultiplierShape_Without_Multiplier() {
+        var subject = new XYChart2() {
+            Canvas = {
+                Width = 1000,
+                Height = 500,
+                Padding = 25,
+                XAxisLabelHeight = 50,
+                XAxisLabelClearance = 5,
+                YAxisLabelWidth = 75,
+                YAxisLabelClearance = 10,
+                YAxisMultiplierFormat = "x 0000"
+            },
+            PlotArea = {
+                 Min = -100M,
+                 Max = 500M,
+                 GridLineInterval = 200M
+            }
+        };
+
+        var result = subject.GetYAxisMultiplierShape();
+
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void GetXAxisLabelShapes() {
+        var subject = new XYChart2() {
+            Canvas = {
+                Width = 1000,
+                Height = 500,
+                Padding = 25,
+                XAxisLabelHeight = 50,
+                XAxisLabelClearance = 5,
+                YAxisLabelWidth = 100,
+                YAxisLabelClearance = 10
+            },
+            PlotArea = {
+                 Min = -100M,
+                 Max = 500M,
+                 GridLineInterval = 200M,
+                 Multiplier = 1000
+            },
+            Labels = { "Foo", "Bar", "Baz" }
+        };
+
+        var result = subject.GetXAxisLabelShapes();
+
+        Assert.Equal(3, result.Count());
+
+        Assert.All(result, shape => {
+            Assert.Equal(500 - 25 - 50 + 5, shape.Y);
+        });
+
+        var dataPointWidth = subject.Canvas.PlotAreaWidth / 3M;
+
+        Assert.Single(result, shape => shape.Key.EndsWith("[0]") && shape.X == subject.Canvas.PlotAreaX + 0.5M * dataPointWidth && shape.Label == "Foo");
+        Assert.Single(result, shape => shape.Key.EndsWith("[1]") && shape.X == subject.Canvas.PlotAreaX + 1.5M * dataPointWidth && shape.Label == "Bar");
+        Assert.Single(result, shape => shape.Key.EndsWith("[2]") && shape.X == subject.Canvas.PlotAreaX + 2.5M * dataPointWidth && shape.Label == "Baz");
+    }
+    
+    [Fact]
+    public void GetDataSeriesShapes() {
+
+        var subject = new XYChart2() {
+            Canvas = {
+                Width = 1000,
+                Height = 500,
+                Padding = 25,
+                XAxisLabelHeight = 50,
+                XAxisLabelClearance = 5,
+                YAxisLabelWidth = 100,
+                YAxisLabelClearance = 10
+            },
+            PlotArea = {
+                    Min = -10M,
+                    Max = 40M,
+                    GridLineInterval = 10M
+            },
+            Labels = { "Foo", "Bar", "Baz" }
+        };
+
+        subject.Layers.Add(new BarLayer2() {
+            Chart = subject,
+            DataSeries = {
+                new() {
+                    Color = "red",
+                    DataPoints = { 5M, null, 15M }
+                }
+            }
+        });
+
+        subject.Layers.Add(new BarLayer2() {
+            Chart = subject,
+            DataSeries = {
+                new() {
+                    Color = "blue",
+                    DataPoints = { 11M, 8M, null }
+                }
+            }
+        });
+
+        var result = subject.GetDataSeriesShapes();
+
+        Assert.Equal(4, result.Count());
+
+        Assert.All(result, shape => Assert.IsType<DataBarShape>(shape));
+    }
+    
     [Theory]
     [MemberData(nameof(MapDataPointToCanvas_Data))]
     public void MapDataPointToCanvas(decimal dataPoint, decimal expectedValue) {
--- BlazorPlayground.Chart/XYChart2.cs
+++ BlazorPlayground.Chart/XYChart2.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using BlazorPlayground.Chart.Shapes;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
 
 namespace BlazorPlayground.Chart;
@@ -17,6 +18,71 @@ public class XYChart2 : ComponentBase {
         builder.AddAttribute(3, "ChildContent", ChildContent);
         builder.CloseComponent();
     }
+
+    public IEnumerable<ShapeBase> GetShapes() {
+        PlotArea.AutoScale(GetScaleDataPoints());
+
+        foreach (var shape in GetGridLineShapes()) {
+            yield return shape;
+        }
+
+        foreach (var shape in GetDataSeriesShapes()) {
+            yield return shape;
+        }
+
+        yield return Canvas.GetPlotAreaShape();
+
+        foreach (var shape in GetYAxisLabelShapes()) {
+            yield return shape;
+        }
+
+        if (GetYAxisMultiplierShape() is ShapeBase multiplierShape) {
+            yield return multiplierShape;
+        }
+
+        foreach (var shape in GetXAxisLabelShapes()) {
+            yield return shape;
+        }
+    }
+
+    public IEnumerable<decimal> GetScaleDataPoints() {
+        var dataPoints = new List<decimal>();
+
+        foreach (var layer in Layers) {
+            // TODO add take label count here
+            dataPoints.AddRange(layer.DataSeries.SelectMany(dataSeries => dataSeries.DataPoints.Where(dataPoint => dataPoint != null).Select(dataPoint => dataPoint!.Value)));
+
+            if (layer.IsStacked) {
+                for (var i = 0; i < Labels.Count; i++) {
+                    // TODO take stack mode into account
+                    if (layer.DataSeries.Any(dataSeries => dataSeries.DataPoints.Count > i && dataSeries.DataPoints[i] < 0)) {
+                        dataPoints.Add(layer.DataSeries.Where(dataSeries => dataSeries.DataPoints.Count > i && dataSeries.DataPoints[i] < 0).Sum(dataSeries => dataSeries.DataPoints[i]!.Value));
+                    }
+
+                    if (layer.DataSeries.Any(dataSeries => dataSeries.DataPoints.Count > i && dataSeries.DataPoints[i] > 0)) {
+                        dataPoints.Add(layer.DataSeries.Where(dataSeries => dataSeries.DataPoints.Count > i && dataSeries.DataPoints[i] > 0).Sum(dataSeries => dataSeries.DataPoints[i]!.Value));
+                    }
+                }
+            }
+        }
+
+        return dataPoints;
+    }
+
+    public IEnumerable<GridLineShape> GetGridLineShapes()
+        => PlotArea.GetGridLineDataPoints().Select((dataPoint, index) => new GridLineShape(Canvas.PlotAreaX, MapDataPointToCanvas(dataPoint), Canvas.PlotAreaWidth, dataPoint, index));
+
+    public IEnumerable<YAxisLabelShape> GetYAxisLabelShapes()
+        => PlotArea.GetGridLineDataPoints().Select((dataPoint, index) => new YAxisLabelShape(Canvas.PlotAreaX - Canvas.YAxisLabelClearance, MapDataPointToCanvas(dataPoint), (dataPoint / PlotArea.Multiplier).ToString(Canvas.YAxisLabelFormat), index));
+
+    public YAxisMultiplierShape? GetYAxisMultiplierShape()
+        => PlotArea.Multiplier == 1M ? null : new YAxisMultiplierShape(Canvas.Padding, Canvas.PlotAreaY + Canvas.PlotAreaHeight / 2M, PlotArea.Multiplier.ToString(Canvas.YAxisMultiplierFormat));
+
+    public IEnumerable<XAxisLabelShape> GetXAxisLabelShapes()
+        => Labels.Select((label, index) => new XAxisLabelShape(MapDataIndexToCanvas(index), Canvas.PlotAreaY + Canvas.PlotAreaHeight + Canvas.XAxisLabelClearance, label, index));
+
+    public IEnumerable<ShapeBase> GetDataSeriesShapes()
+        => Layers.SelectMany(layer => layer.GetDataSeriesShapes());
 
     public decimal MapDataPointToCanvas(decimal dataPoint) => Canvas.PlotAreaY + MapDataValueToPlotArea(PlotArea.Max - dataPoint);
 
