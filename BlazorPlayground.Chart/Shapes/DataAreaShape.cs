db4b33c01890814c00a2d48836f65168ecc9c358
Maikel Bos <maikel.bos0@gmail.com>
2023-08-12 10:34:23 +0200

    Add area shape

---
+++ BlazorPlayground.Chart/Shapes/DataAreaShape.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorPlayground.Chart.Shapes;
+
+public class DataAreaShape : ShapeBase {
+    public override string CssClass => "area-data";
+    public override string ElementName => "path";
+    public string Path { get; }
+    public string Color { get; }
+
+    public DataAreaShape(IEnumerable<string> commands, string color, int dataSeriesIndex) : base(dataSeriesIndex) {
+        Path = string.Join(' ', commands);
+        Color = color;
+    }
+
+    public override ShapeAttributeCollection GetAttributes() => new() {
+        { "d", Path },
+        { "fill", Color }
+    };
+}
