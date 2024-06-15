ccc8e09e182dab2e9cb241c45e1a6d5fbd178929
Maikel Bos <maikel.bos0@gmail.com>
2024-06-15 09:52:38 +0200

    Also get page Y with MATH

--- BlazorPlayground/wwwroot/js/dragToOrder.js
+++ BlazorPlayground/wwwroot/js/dragToOrder.js
@@ -9,4 +9,15 @@ function getElementHeights(container) {
     return heights;
 }
 
-export { getElementHeights }
+function getPageY(container) {
+    let pageY = 0;
+
+    while (container.parentElement != document.body) {
+        pageY += container.offsetTop;
+        container = container.parentElement;
+    }
+
+    return pageY;
+}
+
+export { getElementHeights, getPageY }
