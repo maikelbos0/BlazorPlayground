8dc136fbbc14f3b256946065a0d5ddf1bd091d87
Maikel Bos <maikel.bos0@gmail.com>
2024-06-18 19:48:38 +0200

    Remove unused property

--- BlazorPlayground/wwwroot/js/dragToOrder.js
+++ BlazorPlayground/wwwroot/js/dragToOrder.js
@@ -9,15 +9,4 @@ function getElementHeights(container) {
     return heights;
 }
 
-function getPageY(container) {
-    let pageY = 0;
-
-    while (container.parentElement != document.body) {
-        pageY += container.offsetTop;
-        container = container.parentElement;
-    }
-
-    return pageY;
-}
-
-export { getElementHeights, getPageY }
+export { getElementHeights }
