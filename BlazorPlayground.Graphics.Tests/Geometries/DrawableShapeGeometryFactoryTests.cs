83b5ee7a5bdccbfadb6ccb34e0dfcc2a4bd71230
Maikel Bos <maikel.bos0@gmail.com>
2025-04-10 19:16:42 +0200

    Add rectangle transformation

--- BlazorPlayground.Graphics.Tests/Geometries/DrawableShapeGeometryFactoryTests.cs
+++ BlazorPlayground.Graphics.Tests/Geometries/DrawableShapeGeometryFactoryTests.cs
@@ -1,4 +1,5 @@
 ﻿using BlazorPlayground.Graphics.Geometries;
+using NetTopologySuite.Geometries;
 using System.Linq;
 using Xunit;
 
@@ -7,15 +8,20 @@ namespace BlazorPlayground.Graphics.Tests.Geometries;
 public class DrawableShapeGeometryFactoryTests {
     [Fact]
     public void GetGeometryFromEmptyEnumerable() {
-        var geometryFactory = new DrawableShapeGeometryFactory();
+        var subject = new DrawableShapeGeometryFactory();
 
-        var result = geometryFactory.GetGeometry(Enumerable.Empty<DrawableShape>());
+        var result = subject.GetGeometry(Enumerable.Empty<DrawableShape>());
 
         Assert.Null(result);
     }
 
     [Fact]
     public void GetGeometryFromRectangle() {
+        var subject = new DrawableShapeGeometryFactory();
 
+        var result = subject.GetGeometry([new Rectangle(new(-10, -10), new(50, -30))]);
+
+        Assert.NotNull(result);
+        Assert.Equal(GeometryFactory.Default.CreatePolygon([new(-10, -10), new(-10, -30), new(50, -30), new(50, -10), new(-10, -10)]), result);
     }
 }
--- BlazorPlayground.Graphics/Geometries/DrawableShapeGeometryFactory.cs
+++ BlazorPlayground.Graphics/Geometries/DrawableShapeGeometryFactory.cs
@@ -7,10 +7,22 @@ public class DrawableShapeGeometryFactory {
     public Geometry GetGeometry(IEnumerable<DrawableShape> shapes) {
         var geometries = new List<Geometry>();
 
-        foreach (var  shape in shapes) {
-            
+        foreach (var shape in shapes) {
+            geometries.Add(shape switch {
+                Rectangle rectangle => GetGeometry(rectangle),
+                _ => throw new NotImplementedException()
+            });
         }
 
         return GeometryCombiner.Combine(geometries);
     }
+
+    private Geometry GetGeometry(Rectangle rectangle)
+        => GeometryFactory.Default.CreatePolygon([
+            new(rectangle.StartPoint.X, rectangle.StartPoint.Y),
+            new(rectangle.StartPoint.X, rectangle.EndPoint.Y),
+            new(rectangle.EndPoint.X, rectangle.EndPoint.Y),
+            new(rectangle.EndPoint.X, rectangle.StartPoint.Y),
+            new(rectangle.StartPoint.X, rectangle.StartPoint.Y)
+        ]);
 }
