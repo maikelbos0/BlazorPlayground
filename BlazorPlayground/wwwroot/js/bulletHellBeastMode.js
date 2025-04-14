dce34ae05ab14bea1df881a5c0a56421d6b66055
Maikel Bos <maikel.bos0@gmail.com>
2025-04-14 19:39:07 +0200

    Add JS interop

---
+++ BlazorPlayground/wwwroot/js/bulletHellBeastMode.js
@@ -0,0 +1,11 @@
+﻿
+const game = {};
+
+function initializeGame(width, height, frameRate) {
+    game.canvas = document.getElementById('game');
+    game.canvas.width = width;
+    game.canvas.height = height;
+    game.frameRate = frameRate;
+}
+
+export { initializeGame };
