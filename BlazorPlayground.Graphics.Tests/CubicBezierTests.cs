68ad7ee7734c4271554767095054123ef40c4118
Maikel Bos <maikel.bos0@gmail.com>
2025-04-25 12:08:48 +0200

    Move cubic bezier geometry to cubic bezier class

--- BlazorPlayground.Graphics.Tests/CubicBezierTests.cs
+++ BlazorPlayground.Graphics.Tests/CubicBezierTests.cs
@@ -1,99 +1,126 @@
-﻿using Xunit;
+﻿using NetTopologySuite.Geometries;
+using Xunit;
 
-namespace BlazorPlayground.Graphics.Tests {
-    public class CubicBezierTests {
-        [Fact]
-        public void ElementName() {
-            var bezier = new CubicBezier(new Point(100, 150), new Point(200, 250));
+namespace BlazorPlayground.Graphics.Tests;
 
-            Assert.Equal("path", bezier.ElementName);
-        }
+public class CubicBezierTests {
+    [Fact]
+    public void ElementName() {
+        var bezier = new CubicBezier(new Point(100, 150), new Point(200, 250));
 
-        [Fact]
-        public void GetSnapPoints() {
-            var bezier = new CubicBezier(new Point(100, 150), new Point(200, 250));
+        Assert.Equal("path", bezier.ElementName);
+    }
 
-            var result = bezier.GetSnapPoints();
+    [Fact]
+    public void GetSnapPoints() {
+        var bezier = new CubicBezier(new Point(100, 150), new Point(200, 250));
 
-            Assert.Equal(2, result.Count);
-            PointAssert.Contains(result, new Point(100, 150));
-            PointAssert.Contains(result, new Point(200, 250));
-        }
+        var result = bezier.GetSnapPoints();
 
-        [Fact]
-        public void ControlPoints() {
-            var bezier = new CubicBezier(new Point(100, 150), new Point(200, 250));
+        Assert.Equal(2, result.Count);
+        PointAssert.Contains(result, new Point(100, 150));
+        PointAssert.Contains(result, new Point(200, 250));
+    }
 
-            PointAssert.Equal(new Point(133.333, 183.333), bezier.ControlPoint1);
-            PointAssert.Equal(new Point(166.667, 216.667), bezier.ControlPoint2);
-        }
+    [Fact]
+    public void ControlPoints() {
+        var bezier = new CubicBezier(new Point(100, 150), new Point(200, 250));
 
-        [Fact]
-        public void GetAttributes() {
-            var bezier = new CubicBezier(new Point(100, 150), new Point(200, 250)) {
-                ControlPoint1 = new Point(125, 175),
-                ControlPoint2 = new Point(225, 275)
-            };
+        PointAssert.Equal(new Point(133.333, 183.333), bezier.ControlPoint1);
+        PointAssert.Equal(new Point(166.667, 216.667), bezier.ControlPoint2);
+    }
 
-            var attributes = bezier.GetAttributes();
+    [Fact]
+    public void GetAttributes() {
+        var bezier = new CubicBezier(new Point(100, 150), new Point(200, 250)) {
+            ControlPoint1 = new Point(125, 175),
+            ControlPoint2 = new Point(225, 275)
+        };
 
-            var attribute = Assert.Single(attributes);
-            Assert.Equal("d", attribute.Key);
-            Assert.Equal("M 100 150 C 125 175, 225 275, 200 250", attribute.Value);
-        }
+        var attributes = bezier.GetAttributes();
 
-        [Fact]
-        public void Anchors_Get() {
-            var bezier = new CubicBezier(new Point(100, 150), new Point(200, 250)) {
-                ControlPoint1 = new Point(125, 175),
-                ControlPoint2 = new Point(225, 275)
-            };
+        var attribute = Assert.Single(attributes);
+        Assert.Equal("d", attribute.Key);
+        Assert.Equal("M 100 150 C 125 175, 225 275, 200 250", attribute.Value);
+    }
 
-            var result = bezier.Anchors;
+    [Fact]
+    public void Anchors_Get() {
+        var bezier = new CubicBezier(new Point(100, 150), new Point(200, 250)) {
+            ControlPoint1 = new Point(125, 175),
+            ControlPoint2 = new Point(225, 275)
+        };
 
-            Assert.Equal(4, result.Count);
-            PointAssert.Equal(new Point(100, 150), result[0].Get(bezier));
-            PointAssert.Equal(new Point(125, 175), result[1].Get(bezier));
-            PointAssert.Equal(new Point(225, 275), result[2].Get(bezier));
-            PointAssert.Equal(new Point(200, 250), result[3].Get(bezier));
-        }
+        var result = bezier.Anchors;
 
-        [Fact]
-        public void Anchors_Set() {
-            var bezier = new CubicBezier(new Point(100, 150), new Point(200, 250)) {
-                ControlPoint1 = new Point(125, 175),
-                ControlPoint2 = new Point(225, 275)
-            };
+        Assert.Equal(4, result.Count);
+        PointAssert.Equal(new Point(100, 150), result[0].Get(bezier));
+        PointAssert.Equal(new Point(125, 175), result[1].Get(bezier));
+        PointAssert.Equal(new Point(225, 275), result[2].Get(bezier));
+        PointAssert.Equal(new Point(200, 250), result[3].Get(bezier));
+    }
 
-            var result = bezier.Anchors;
+    [Fact]
+    public void Anchors_Set() {
+        var bezier = new CubicBezier(new Point(100, 150), new Point(200, 250)) {
+            ControlPoint1 = new Point(125, 175),
+            ControlPoint2 = new Point(225, 275)
+        };
 
-            Assert.Equal(4, result.Count);
-            result[0].Set(bezier, new Point(110, 160));
-            result[1].Set(bezier, new Point(135, 185));
-            result[2].Set(bezier, new Point(235, 285));
-            result[3].Set(bezier, new Point(210, 260));
-            PointAssert.Equal(new Point(110, 160), bezier.StartPoint);
-            PointAssert.Equal(new Point(135, 185), bezier.ControlPoint1);
-            PointAssert.Equal(new Point(235, 285), bezier.ControlPoint2);
-            PointAssert.Equal(new Point(210, 260), bezier.EndPoint);
-        }
+        var result = bezier.Anchors;
 
-        [Fact]
-        public void Clone() {
-            var bezier = new CubicBezier(new Point(100, 150), new Point(200, 250)) {
-                ControlPoint1 = new Point(125, 175),
-                ControlPoint2 = new Point(225, 275)
-            };
+        Assert.Equal(4, result.Count);
+        result[0].Set(bezier, new Point(110, 160));
+        result[1].Set(bezier, new Point(135, 185));
+        result[2].Set(bezier, new Point(235, 285));
+        result[3].Set(bezier, new Point(210, 260));
+        PointAssert.Equal(new Point(110, 160), bezier.StartPoint);
+        PointAssert.Equal(new Point(135, 185), bezier.ControlPoint1);
+        PointAssert.Equal(new Point(235, 285), bezier.ControlPoint2);
+        PointAssert.Equal(new Point(210, 260), bezier.EndPoint);
+    }
 
-            var result = bezier.Clone();
+    [Fact]
+    public void Clone() {
+        var bezier = new CubicBezier(new Point(100, 150), new Point(200, 250)) {
+            ControlPoint1 = new Point(125, 175),
+            ControlPoint2 = new Point(225, 275)
+        };
 
-            var resultBezier = Assert.IsType<CubicBezier>(result);
+        var result = bezier.Clone();
 
-            Assert.NotSame(bezier, resultBezier);
-            PointAssert.Equal(new Point(100, 150), resultBezier.StartPoint);
-            PointAssert.Equal(new Point(125, 175), resultBezier.ControlPoint1);
-            PointAssert.Equal(new Point(225, 275), resultBezier.ControlPoint2);
-            PointAssert.Equal(new Point(200, 250), resultBezier.EndPoint);
-        }
+        var resultBezier = Assert.IsType<CubicBezier>(result);
+
+        Assert.NotSame(bezier, resultBezier);
+        PointAssert.Equal(new Point(100, 150), resultBezier.StartPoint);
+        PointAssert.Equal(new Point(125, 175), resultBezier.ControlPoint1);
+        PointAssert.Equal(new Point(225, 275), resultBezier.ControlPoint2);
+        PointAssert.Equal(new Point(200, 250), resultBezier.EndPoint);
+    }
+
+    [Fact]
+    public void GetGeometry() {
+        var geometryFactory = new GeometryFactory(new PrecisionModel(10));
+        var subject = new CubicBezier(new(30, 50), new(50, 100)) {
+            ControlPoint1 = new(20, 60),
+            ControlPoint2 = new(10, 90)
+        };
+
+        var result = subject.GetGeometry(geometryFactory, new(-100, -100));
+
+        Assert.Equal(geometryFactory.CreateLineString([new(130, 150), new(129.5, 150.5), new(129, 151.1), new(128.5, 151.6), new(128, 152.3), new(127.5, 152.9), new(127.1, 153.6), new(126.6, 154.3), new(126.1, 155), new(125.7, 155.7), new(125.2, 156.5), new(124.8, 157.3), new(124.4, 158.1), new(124, 158.9), new(123.6, 159.8), new(123.3, 160.6), new(122.9, 161.5), new(122.6, 162.4), new(122.4, 163.3), new(122.1, 164.2), new(121.9, 165.2), new(121.6, 166.1), new(121.5, 167.1), new(121.3, 168.1), new(121.2, 169), new(121.1, 170), new(121.1, 171), new(121.1, 172), new(121.1, 173), new(121.1, 174), new(121.3, 175), new(121.4, 176), new(121.6, 177), new(121.8, 178), new(122.1, 179), new(122.4, 180), new(122.8, 181), new(123.2, 181.9), new(123.7, 182.9), new(124.2, 183.9), new(124.8, 184.8), new(125.5, 185.8), new(126.2, 186.7), new(126.9, 187.6), new(127.7, 188.5), new(128.6, 189.4), new(129.5, 190.2), new(130.5, 191.1), new(131.6, 191.9), new(132.7, 192.7), new(133.9, 193.5), new(135.2, 194.3), new(136.5, 195), new(138, 195.7), new(139.5, 196.4), new(141, 197.1), new(142.7, 197.7), new(144.4, 198.4), new(146.2, 198.9), new(148, 199.5), new(150, 200)]), result);
+    }
+
+    [Fact]
+    public void GetBoundingBox() {
+        var subject = new CubicBezier(new(50, 100), new(30, 50)) {
+            ControlPoint1 = new(20, 60),
+            ControlPoint2 = new(10, 90)
+        };
+
+        var result = subject.GetBoundingBox();
+
+        BoundingBoxAssert.Equal(new(30, 50, 50, 100), result);
+
     }
 }
--- BlazorPlayground.Graphics/CubicBezier.cs
+++ BlazorPlayground.Graphics/CubicBezier.cs
@@ -1,40 +1,70 @@
-﻿namespace BlazorPlayground.Graphics {
-    public class CubicBezier : DrawableShape, IShapeWithOpacity, IShapeWithFill, IShapeWithStroke, IShapeWithStrokeLinecap, IAutoSelectedShape {
-        private readonly static Anchor[] anchors = new[] {
-            new Anchor<CubicBezier>(s => s.StartPoint, (s, p) => s.StartPoint = p),
-            new Anchor<CubicBezier>(s => s.ControlPoint1, (s, p) => s.ControlPoint1 = p),
-            new Anchor<CubicBezier>(s => s.ControlPoint2, (s, p) => s.ControlPoint2 = p),
-            new Anchor<CubicBezier>(s => s.EndPoint, (s, p) => s.EndPoint = p)
-        };
+﻿using NetTopologySuite.Geometries;
 
-        public override string ElementName => "path";
-        public override IReadOnlyList<Anchor> Anchors { get; } = Array.AsReadOnly(anchors);
-        public Point StartPoint { get; set; }
-        public Point ControlPoint1 { get; set; }
-        public Point ControlPoint2 { get; set; }
-        public Point EndPoint { get; set; }
+namespace BlazorPlayground.Graphics;
 
-        private CubicBezier() : this(new Point(0, 0), new Point(0, 0)) { }
+public class CubicBezier : DrawableShape, IShapeWithOpacity, IShapeWithFill, IShapeWithStroke, IShapeWithStrokeLinecap, IAutoSelectedShape {
+    private readonly static Anchor[] anchors = [
+        new Anchor<CubicBezier>(s => s.StartPoint, (s, p) => s.StartPoint = p),
+        new Anchor<CubicBezier>(s => s.ControlPoint1, (s, p) => s.ControlPoint1 = p),
+        new Anchor<CubicBezier>(s => s.ControlPoint2, (s, p) => s.ControlPoint2 = p),
+        new Anchor<CubicBezier>(s => s.EndPoint, (s, p) => s.EndPoint = p)
+    ];
 
-        public CubicBezier(Point startPoint, Point endPoint) {
-            StartPoint = startPoint;
-            EndPoint = endPoint;
+    public override string ElementName => "path";
+    public override IReadOnlyList<Anchor> Anchors { get; } = Array.AsReadOnly(anchors);
+    public Point StartPoint { get; set; }
+    public Point ControlPoint1 { get; set; }
+    public Point ControlPoint2 { get; set; }
+    public Point EndPoint { get; set; }
 
-            var step = (EndPoint - startPoint) / 3;
+    private CubicBezier() : this(new Point(0, 0), new Point(0, 0)) { }
 
-            ControlPoint1 = StartPoint + step;
-            ControlPoint2 = StartPoint + step * 2;
+    public CubicBezier(Point startPoint, Point endPoint) {
+        StartPoint = startPoint;
+        EndPoint = endPoint;
+
+        var step = (EndPoint - startPoint) / 3;
+
+        ControlPoint1 = StartPoint + step;
+        ControlPoint2 = StartPoint + step * 2;
+    }
+
+    public override IReadOnlyList<Point> GetSnapPoints() => Array.AsReadOnly([StartPoint, EndPoint]);
+
+    public override ShapeAttributeCollection GetAttributes() => new() {
+        { "d", FormattableString.Invariant($"M {StartPoint.X} {StartPoint.Y} C {ControlPoint1.X} {ControlPoint1.Y}, {ControlPoint2.X} {ControlPoint2.Y}, {EndPoint.X} {EndPoint.Y}") }
+    };
+
+    protected override Shape CreateClone() => new CubicBezier(StartPoint, EndPoint) {
+        ControlPoint1 = ControlPoint1,
+        ControlPoint2 = ControlPoint2
+    };
+
+    public override Geometry GetGeometry(GeometryFactory geometryFactory, Point origin) {
+        var coordinates = new Coordinate[CurveApproximationSegmentCount + 1];
+
+        for (var i = 1; i < CurveApproximationSegmentCount; i++) {
+            var step = CurveAproximationStepIncrement * i;
+            var invertedStep = 1.0 - step;
+            var intermediatePoint = StartPoint * invertedStep * invertedStep * invertedStep
+                + ControlPoint1 * step * invertedStep * invertedStep * 3
+                + ControlPoint2 * step * step * invertedStep * 3
+                + EndPoint * step * step * step;
+
+            coordinates[i] = geometryFactory.GetCoordinate(intermediatePoint.X, intermediatePoint.Y, origin);
         }
 
-        public override IReadOnlyList<Point> GetSnapPoints() => Array.AsReadOnly(new[] { StartPoint, EndPoint });
+        coordinates[0] = geometryFactory.GetCoordinate(StartPoint.X, StartPoint.Y, origin);
+        coordinates[^1] = geometryFactory.GetCoordinate(EndPoint.X, EndPoint.Y, origin);
 
-        public override ShapeAttributeCollection GetAttributes() => new() {
-            { "d", FormattableString.Invariant($"M {StartPoint.X} {StartPoint.Y} C {ControlPoint1.X} {ControlPoint1.Y}, {ControlPoint2.X} {ControlPoint2.Y}, {EndPoint.X} {EndPoint.Y}") }
-        };
-
-        protected override Shape CreateClone() => new CubicBezier(StartPoint, EndPoint) {
-            ControlPoint1 = ControlPoint1,
-            ControlPoint2 = ControlPoint2
-        };
+        return geometryFactory.CreateLineString(coordinates);
     }
+
+    public override BoundingBox GetBoundingBox()
+        => new(
+            Math.Min(StartPoint.X, EndPoint.X),
+            Math.Max(StartPoint.X, EndPoint.X),
+            Math.Min(StartPoint.Y, EndPoint.Y),
+            Math.Max(StartPoint.Y, EndPoint.Y)
+        );
 }
