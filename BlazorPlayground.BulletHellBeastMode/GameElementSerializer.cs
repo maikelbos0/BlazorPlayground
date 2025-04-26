6eae9a1bc2d362775b44e23aa83567de12f192e5
Maikel Bos <maikel.bos0@gmail.com>
2025-04-26 11:11:09 +0200

    Add save option for game elements

---
+++ BlazorPlayground.BulletHellBeastMode/GameElementSerializer.cs
@@ -0,0 +1,21 @@
+﻿using NetTopologySuite.IO.Converters;
+using System.Text.Json;
+
+namespace BlazorPlayground.BulletHellBeastMode;
+
+public static class GameElementSerializer {
+    private readonly static JsonSerializerOptions jsonSerializerOptions = new() {
+        Converters = {
+            new GeoJsonConverterFactory()
+        }
+    };
+
+    public static GameElement Deserialize(string serializedGameElement) {
+        return JsonSerializer.Deserialize<GameElement>(serializedGameElement, jsonSerializerOptions) 
+            ?? throw new NullReferenceException();
+    }
+
+    public static string Serialize(GameElement gameElement) {
+        return JsonSerializer.Serialize(gameElement, jsonSerializerOptions);
+    }
+}
