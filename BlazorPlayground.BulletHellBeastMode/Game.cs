1ff8c73cf841daf2f0355af98461874b4eedf380
Maikel Bos <maikel.bos0@gmail.com>
2025-04-27 13:30:32 +0200

    Move game stuff to game module and init with rendered canvas

---
+++ BlazorPlayground.BulletHellBeastMode/Game.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.JSInterop;
+
+namespace BlazorPlayground.BulletHellBeastMode;
+
+public class Game : ComponentBase, IAsyncDisposable {
+    public const int Width = 1000;
+    public const int Height = 1000;
+    public const int FrameRate = 1000;
+    public const string ModuleLocation = "./_content/BlazorPlayground.BulletHellBeastMode/bulletHellBeastMode.0.js";
+
+    private readonly Guid id = Guid.NewGuid();
+    private IJSObjectReference? moduleReference;
+    private DotNetObjectReference<Game>? dotNetObjectReference;
+
+    [Inject] internal IJSRuntime JSRuntime { get; set; } = null!;
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder) {
+        builder.OpenElement(0, "canvas");
+        builder.AddAttribute(1, "id", id);
+        builder.CloseElement();
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender) {
+        if (firstRender) {
+            moduleReference = await JSRuntime.InvokeAsync<IJSObjectReference>("import", ModuleLocation);
+            dotNetObjectReference = DotNetObjectReference.Create(this);
+            await moduleReference.InvokeVoidAsync("initialize", id, Width, Height, FrameRate);
+        }
+    }
+
+    public async ValueTask DisposeAsync() {
+        if (moduleReference != null) {
+            await moduleReference.DisposeAsync();
+        }
+
+        dotNetObjectReference?.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ BlazorPlayground.BulletHellBeastMode/wwwroot/bulletHellBeastMode.0.js
@@ -0,0 +1,12 @@
+﻿
+const game = {};
+
+function initialize(id, width, height, frameRate) {
+    game.canvas = document.getElementById(id);
+    game.canvas.width = width;
+    game.canvas.height = height;
+    game.frameRate = frameRate;
+    game.elements = {};
+}
+
+export { initialize };
