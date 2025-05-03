fd416be76760d0a5787eaa786de4aebbee2a2b1e
Maikel Bos <maikel.bos0@gmail.com>
2025-05-03 08:37:15 +0200

    Render elements

--- BlazorPlayground.BulletHellBeastMode/wwwroot/game.0.js
+++ BlazorPlayground.BulletHellBeastMode/wwwroot/game.0.js
@@ -2,9 +2,12 @@
 const game = {};
 
 function initialize(canvas, dotNetObjectReference, width, height) {
+    canvas.width = width;
+    canvas.height = height;
+
     game.canvas = canvas;
-    game.canvas.width = width;
-    game.canvas.height = height;
+    game.context = canvas.getContext("2d");
+    game.context.globalCompositeOperation = "destination-over";
     game.dotNetObjectReference = dotNetObjectReference;
     game.elements = {};
 }
@@ -14,7 +17,36 @@ function requestRender() {
 }
 
 function render(timestamp) {
-    console.log("Rendering at: " + timestamp);
+    game.context.clearRect(0, 0, game.canvas.width, game.canvas.height);
+
+    for (let id in game.elements) {
+        let element = game.elements[id];
+
+        game.context.save();
+        game.context.translate(element.position.x, element.position.y);
+
+        element.sections.forEach((section) => {
+            game.context.beginPath();
+            game.context.fillStyle = section.fillColor;
+            game.context.strokeStyle = section.strokeColor;
+            game.context.lineWidth = section.strokeWidth;
+            game.context.globalAlpha = section.opacity;
+
+            section.coordinates.forEach((coordinate, index) => {
+                if (index === 0) {
+                    game.context.moveTo(coordinate.x, coordinate.y);
+                }
+                else {
+                    game.context.lineTo(coordinate.x, coordinate.y);
+                }
+            });;
+
+            game.context.fill();
+            game.context.stroke();
+        })
+
+        game.context.restore();
+    }
 
     if (game.previousTimestamp) {
         game.dotNetObjectReference.invokeMethodAsync("ProcessElapsedTime", timestamp - game.previousTimestamp)
