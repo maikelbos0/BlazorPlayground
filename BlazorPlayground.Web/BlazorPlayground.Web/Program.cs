6298c182980301eab1815000c74c3c4a4f6b424c
Maikel Bos <maikel.bos0@gmail.com>
2025-07-18 09:46:08 +0200

    Add request logging

--- BlazorPlayground.Web/BlazorPlayground.Web/Program.cs
+++ BlazorPlayground.Web/BlazorPlayground.Web/Program.cs
@@ -1,27 +1,27 @@
-using BlazorPlayground.Web.Client.Pages;
 using BlazorPlayground.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
 
+
+builder.Services.AddHttpLogging(o => { });
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
     app.UseWebAssemblyDebugging();
 }
 else {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+;
 
+app.UseHttpLogging();
 app.UseHttpsRedirection();
-
 
 app.UseAntiforgery();
 
