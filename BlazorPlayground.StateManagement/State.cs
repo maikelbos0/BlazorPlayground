8cf747c05c0e44c0f37b5b4eefcb5f7a7f946313
Maikel Bos <maikel.bos0@gmail.com>
2025-07-18 13:30:48 +0200

    Add simple state class

---
+++ BlazorPlayground.StateManagement/State.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorPlayground.StateManagement;
+
+public class State<T> {
+    public State(T value) {
+        Value = value;
+    }
+
+    public T Value { get; private set; }
+
+    public void Set(T value) {
+        Value = value;
+    }
+
+    public void Update(ValueProvider<T> valueProvider) {
+        Value = valueProvider(Value);
+    }
+}
+
+public delegate T ValueProvider<T>(T currentValue);
