6a53557cf8412e453df9a212b9f5ef2a6fa526f8
Maikel Bos <maikel.bos0@gmail.com>
2025-07-20 08:36:55 +0200

    Add computed state

---
+++ BlazorPlayground.StateManagement.Tests/ComputedStateTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace BlazorPlayground.StateManagement.Tests;
+
+public class ComputedStateTests {
+    [Fact]
+    public void Constructor() {
+        var stateProvider = new StateProvider();
+        var state = new MutableState<int>(stateProvider, 42);
+        var subject = new ComputedState<int>(stateProvider, () => state.Value);
+
+        Assert.Equal(42, subject.Value);
+    }
+
+    [Fact]
+    public void Evaluate() {
+        var stateProvider = new StateProvider();
+        var state = new MutableState<int>(stateProvider, 41);
+        var subject = new ComputedState<int>(stateProvider, () => state.Value);
+
+        state.Set(42);
+
+        subject.Evaluate();
+
+        Assert.Equal(42, subject.Value);
+    }
+}
---
+++ BlazorPlayground.StateManagement/ComputedState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BlazorPlayground.StateManagement;
+
+public class ComputedState<T> : State<T> {
+    private readonly Func<T> computation;
+    private readonly object valueLock = new();
+    private T value;
+
+    public override T Value => value;
+
+    public ComputedState(StateProvider stateProvider, Func<T> computation) : base(stateProvider) {
+        this.computation = computation;
+        value = computation();
+    }
+
+    public void Evaluate() {
+        lock (valueLock) {
+            value = computation();
+        }
+    }
+}
