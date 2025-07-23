9b075d0115b6ba66ae50f14d26b8fd0be68ea7c5
Maikel Bos <maikel.bos0@gmail.com>
2025-07-23 20:33:18 +0200

    Add effect class for void results

---
+++ BlazorPlayground.StateManagement.Tests/EffectTests.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+
+namespace BlazorPlayground.StateManagement.Tests;
+
+public class EffectTests {
+    [Fact]
+    public void Constructor() {
+        var result = 41;
+        var stateProvider = new StateProvider();
+        var mutableState = new MutableState<int>(stateProvider, 42);
+        var subject = new Effect(stateProvider, () => result = mutableState.Value);
+
+        Assert.Equal(42, result);
+        Assert.Equal(subject, Assert.Single(((IDependency)mutableState).Dependents));
+    }
+
+    [Fact]
+    public void Evaluate() {
+        var result = 41;
+        var stateProvider = new StateProvider();
+        var mutableState = new MutableState<int>(stateProvider, 41);
+        var subject = new Effect(stateProvider, () => result = mutableState.Value);
+
+        mutableState.Set(42);
+
+        ((IDependent)subject).Evaluate();
+
+        Assert.Equal(42, result);
+    }
+}
---
+++ BlazorPlayground.StateManagement/Effect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BlazorPlayground.StateManagement;
+
+public class Effect : IDependent {
+    private readonly Action effect;
+
+    internal Effect(StateProvider stateProvider, Action effect) {
+        this.effect = effect;
+
+        stateProvider.StartBuildingDependencyGraph(this);
+        effect();
+        stateProvider.StopBuildingDependencyGraph(this);
+    }
+
+    public void Evaluate() => effect();
+}
