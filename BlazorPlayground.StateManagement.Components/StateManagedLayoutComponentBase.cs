2df71fd31bc2cfb0c7abbc7cb071f8d9c55daefb
Maikel Bos <maikel.bos0@gmail.com>
2025-07-26 12:07:19 +0200

    Added layout component as well

---
+++ BlazorPlayground.StateManagement.Components/StateManagedLayoutComponentBase.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorPlayground.StateManagement.Components;
+
+public abstract class StateManagedLayoutComponentBase : LayoutComponentBase, IHandleEvent, IDependent, IDisposable {
+    private IDependencyGraphBuilder? dependencyGraphBuilder;
+    private bool isDisposed = false;
+
+    [Parameter]
+    public required IStateProvider StateProvider { get; set; }
+
+    Task IHandleEvent.HandleEventAsync(EventCallbackWorkItem item, object? arg)
+        => item.InvokeAsync(arg);
+
+    protected override void OnInitialized()
+        => dependencyGraphBuilder = StateProvider.GetDependencyGraphBuilder(this);
+
+    protected override void OnAfterRender(bool firstRender) {
+        if (firstRender) {
+            dependencyGraphBuilder?.Dispose();
+        }
+    }
+
+    public void Evaluate() => StateHasChanged();
+
+    public void Dispose() {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing) {
+        if (!isDisposed) {
+            isDisposed = true;
+
+            if (disposing) {
+                dependencyGraphBuilder?.Dispose();
+            }
+        }
+    }
+}
