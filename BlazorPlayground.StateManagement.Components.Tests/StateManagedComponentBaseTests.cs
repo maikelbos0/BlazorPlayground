19e7f17a00f5ee8e225b2c5915de17b77c052986
Maikel Bos <maikel.bos0@gmail.com>
2025-08-01 09:02:59 +0200

    Add proper tests for component

--- BlazorPlayground.StateManagement.Components.Tests/StateManagedComponentBaseTests.cs
+++ BlazorPlayground.StateManagement.Components.Tests/StateManagedComponentBaseTests.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
 using NSubstitute;
 using System;
+using System.Reflection;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -9,6 +11,8 @@ namespace BlazorPlayground.StateManagement.Components.Tests;
 public class StateManagedComponentBaseTests {
     private class StateManagedComponent : StateManagedComponentBase {
         public new void StateHasChanged() => base.StateHasChanged();
+
+        public new bool ShouldRender() => base.ShouldRender();
     }
 
     [Fact]
@@ -26,8 +30,25 @@ public class StateManagedComponentBaseTests {
         Assert.Equal(42, result);
     }
 
+    [Theory]
+    [InlineData(true, true)]
+    [InlineData(false, false)]
+    public void ShouldRender_Returns_Correct_Value(bool isEvaluating, bool expectedShouldRender) {
+        var stateProvider = Substitute.For<IStateProvider>();
+        var subject = new StateManagedComponent() {
+            StateProvider = stateProvider
+        };
+
+        var isEvaluatingInfo = typeof(StateManagedComponentBase).GetField("isEvaluating", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        Assert.NotNull(isEvaluatingInfo);
+        isEvaluatingInfo.SetValue(subject, isEvaluating);
+
+        Assert.Equal(expectedShouldRender, subject.ShouldRender());
+    }
+
     [Fact]
-    public void StateHasChanged_Builds_DependencyGraph() {
+    public void RenderFragment_Builds_DependencyGraph() {
         var stateProvider = Substitute.For<IStateProvider>();
         var dependencyGraphBuilder = Substitute.For<IDependencyGraphBuilder>();
         stateProvider.GetDependencyGraphBuilder(Arg.Any<IDependent>()).Returns(dependencyGraphBuilder);
@@ -36,7 +57,14 @@ public class StateManagedComponentBaseTests {
             StateProvider = stateProvider
         };
 
-        subject.StateHasChanged();
+        var renderFragmentInfo = typeof(ComponentBase).GetField("_renderFragment", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        Assert.NotNull(renderFragmentInfo);
+
+        var renderFragment = Assert.IsType<RenderFragment>(renderFragmentInfo.GetValue(subject));
+
+        Assert.NotNull(renderFragment);
+        renderFragment(new RenderTreeBuilder());
 
         stateProvider.Received(1).GetDependencyGraphBuilder(subject);
         dependencyGraphBuilder.Received(1).Dispose();
