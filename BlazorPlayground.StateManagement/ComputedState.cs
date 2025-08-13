a8a9bbad94b743f55fcfc3d0b9e3e13aa6aab0af
Maikel Bos <maikel.bos0@gmail.com>
2025-08-13 17:38:13 +0200

    Remove failed attempt at removing nullable warning

--- BlazorPlayground.StateManagement/ComputedState.cs
+++ BlazorPlayground.StateManagement/ComputedState.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 
 namespace BlazorPlayground.StateManagement;
@@ -8,7 +7,7 @@ public class ComputedState<T> : DependencyBase, IDependent {
     private readonly StateProvider stateProvider;
     private readonly Func<T> computation;
     private readonly Lock valueLock = new();
-    private T value;
+    private T value = default!;
 
     public T Value {
         get {
@@ -20,12 +19,6 @@ public class ComputedState<T> : DependencyBase, IDependent {
     public ComputedState(StateProvider stateProvider, Func<T> computation) {
         this.stateProvider = stateProvider;
         this.computation = computation;
-
-        BuildDependencyGraph();
-    }
-
-    [MemberNotNull(nameof(value))]
-    private void BuildDependencyGraph() {
         stateProvider.BuildDependencyGraph(this, () => value = computation());
     }
 
