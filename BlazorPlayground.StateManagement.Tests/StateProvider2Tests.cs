f02af41370d7621659d9e82b3e98f112ca5c7146
Maikel Bos <maikel.bos0@gmail.com>
2025-08-18 10:58:58 +0200

    Add convenience methods

--- BlazorPlayground.StateManagement.Tests/StateProvider2Tests.cs
+++ BlazorPlayground.StateManagement.Tests/StateProvider2Tests.cs
@@ -1,16 +1,56 @@
 ﻿using NSubstitute;
+using System.Collections.Generic;
 using Xunit;
 
 namespace BlazorPlayground.StateManagement.Tests;
 
-public class StateProvider2Tests { 
+public class StateProvider2Tests {
+    [Fact]
+    public void Mutable() {
+        var subject = new StateProvider2();
+
+        var result = subject.Mutable(42);
+
+        Assert.Equal(42, result.Value);
+    }
+
+    [Fact]
+    public void Mutable_With_IEqualityComparer() {
+        var subject = new StateProvider2();
+
+        var result = subject.Mutable(42, Substitute.For<IEqualityComparer<int>>());
+
+        Assert.Equal(42, result.Value);
+    }
+
+    [Fact]
+    public void Computed() {
+        var subject = new StateProvider2();
+        var mutableState = subject.Mutable(42);
+
+        var result = subject.Computed(() => mutableState.Value);
+
+        Assert.Equal(42, result.Value);
+    }
+
+    [Fact]
+    public void Effect() {
+        var result = 41;
+        var subject = new StateProvider2();
+        var mutableState = subject.Mutable(42);
+
+        subject.Effect(() => result = mutableState.Value);
+
+        Assert.Equal(42, result);
+    }
+
     [Fact]
     public void IncrementVersion() {
         var subject = new StateProvider2();
         var version = subject.Version;
 
         var result = subject.IncrementVersion();
-        
+
         Assert.Equal(version + 1, subject.Version);
         Assert.Equal(subject.Version, result);
     }
--- BlazorPlayground.StateManagement/StateProvider2.cs
+++ BlazorPlayground.StateManagement/StateProvider2.cs
@@ -11,6 +11,18 @@ public class StateProvider2 {
 
     public uint Version => version;
 
+    public MutableState2<T> Mutable<T>(T value)
+        => new(this, value);
+
+    public MutableState2<T> Mutable<T>(T value, IEqualityComparer<T> equalityComparer)
+        => new(this, value, equalityComparer);
+
+    public ComputedState2<T> Computed<T>(Func<T> computation)
+        => new(this, computation);
+
+    public void Effect(Action effect)
+        => _ = new Effect2(this, effect);
+
     public uint IncrementVersion() => Interlocked.Increment(ref version);
 
     public void BuildDependencyGraph(IDependent2 dependent, Action action) {
