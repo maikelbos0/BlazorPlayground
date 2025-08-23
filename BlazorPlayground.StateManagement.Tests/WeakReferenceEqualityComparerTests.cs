7f212bbd93895ad76b8c8d7151d30cf95ae2939d
Maikel Bos <maikel.bos0@gmail.com>
2025-08-23 08:31:51 +0200

    Add equality comparer for weak references to objects

---
+++ BlazorPlayground.StateManagement.Tests/WeakReferenceEqualityComparerTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Xunit;
+
+namespace BlazorPlayground.StateManagement.Tests;
+
+public class WeakReferenceEqualityComparerTests {
+    private class Target {
+        public int HashCode { get; }
+
+        public Target(int hashCode) {
+            HashCode = hashCode;
+        }
+
+        public override int GetHashCode() => HashCode;
+    }
+
+    [Fact]
+    public void Equals_Null_WeakReferences() {
+        var subject = WeakReferenceEqualityComparer<Target>.Instance;
+
+        Assert.True(subject.Equals(null, null));
+    }
+
+    [Fact]
+    public void Equals_Same_WeakReference() {
+        var subject = WeakReferenceEqualityComparer<Target>.Instance;
+        var weakReference = new WeakReference<Target>(new Target(42));
+
+        Assert.True(subject.Equals(weakReference, weakReference));
+    }
+
+    [Fact]
+    public void Equals_Same_Target() {
+        var subject = WeakReferenceEqualityComparer<Target>.Instance;
+        var target = new Target(42);
+
+        Assert.True(subject.Equals(new WeakReference<Target>(target), new WeakReference<Target>(target)));
+    }
+
+    [Fact]
+    public void Equals_Null_Targets() {
+        var subject = WeakReferenceEqualityComparer<Target>.Instance;
+
+        Assert.True(subject.Equals(new WeakReference<Target>(null!), new WeakReference<Target>(null!)));
+    }
+
+    [Fact]
+    public void Equals_Different_Target() {
+        var subject = WeakReferenceEqualityComparer<Target>.Instance;
+        var weakReferenceX = new WeakReference<Target>(new Target(42));
+        var weakReferenceY = new WeakReference<Target>(new Target(42));
+
+        Assert.False(subject.Equals(weakReferenceX, weakReferenceY));
+    }
+
+    [Fact]
+    public void GetHashCode_Returns_Target_HashCode() {
+        var subject = WeakReferenceEqualityComparer<Target>.Instance;
+        var weakReference = new WeakReference<Target>(new Target(42));
+
+        Assert.Equal(42, subject.GetHashCode(weakReference));
+    }
+
+    [Fact]
+    public void GetHashCode_Returns_Zero_For_Null_Target() {
+        var subject = WeakReferenceEqualityComparer<Target>.Instance;
+        var weakReference = new WeakReference<Target>(null!);
+
+        Assert.Equal(0, subject.GetHashCode(weakReference));
+    }
+}
---
+++ BlazorPlayground.StateManagement/WeakReferenceEqualityComparer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BlazorPlayground.StateManagement;
+
+public class WeakReferenceEqualityComparer<T> : IEqualityComparer<WeakReference<T>> where T : class {
+    public static WeakReferenceEqualityComparer<T> Instance { get; } = new();
+
+    public bool Equals(WeakReference<T>? x, WeakReference<T>? y) {
+        if (x == y) return true;
+
+        T? targetX = null;
+        T? targetY = null;
+
+        _ = x?.TryGetTarget(out targetX);
+        _ = y?.TryGetTarget(out targetY);
+
+        return targetX == targetY;
+    }
+
+    public int GetHashCode([DisallowNull] WeakReference<T> obj) {
+        if (obj.TryGetTarget(out var target)) {
+            return target.GetHashCode();
+        }
+
+        return 0;
+    }
+}
